235be1ff6322d7a7683e43261e2a72e0a82312a9
Oscar Ramirez <oscar@rinro.com.mx>
2019-09-24 15:15:34 -0500

    Bubble charts part 2, exportable Charts to png

--- ChatDrawer.js
+++ ChatDrawer.js
@@ -150,7 +150,8 @@ function copyTextToClipboard(text) {
         document.addEventListener('click',function(e){
             if(e.target){
 
-                if(e.target.classList.contains('bar') || e.target.classList.contains('line-dot') || e.target.classList.contains('square')){
+                if(e.target.classList.contains('bar') || e.target.classList.contains('line-dot')
+                || e.target.classList.contains('square') || e.target.classList.contains('circle')){
                     var selectedBars = e.target.parentElement.getElementsByClassName('active');
                     for (var i = 0; i < selectedBars.length; i++) {
                         selectedBars[i].classList.remove('active');
@@ -348,6 +349,44 @@ function copyTextToClipboard(text) {
                     ChatDrawer.createHeatmap(component, labelsX, labelsY, values, col1, col2, col3);
                     ChatDrawer.refreshToolbarButtons(component, 'heatmap');
                 }
+
+                if(e.target.classList.contains('bubble_chart')){
+                    if(e.target.tagName == 'BUTTON'){
+                        var idRequest = e.target.dataset.id;
+                    }
+                    else if(e.target.tagName == 'svg'){
+                        var idRequest = e.target.parentElement.dataset.id;
+                    }else{
+                        var idRequest = e.target.parentElement.parentElement.dataset.id;
+                    }
+                    var json = ChatDrawer.responses[idRequest];
+                    var component = document.querySelectorAll(`[data-componentid='${idRequest}']`)[0];
+                    var values = ChatDrawer.formatDataToHeatmap(json);
+                    var labelsX = ChatDrawer.getUniqueValues(values, row => row.labelX);
+                    var labelsY = ChatDrawer.getUniqueValues(values, row => row.labelY);
+
+                    var col1 = ChatDrawer.formatColumnName(json['columns'][0]['name']);
+                    var col2 = ChatDrawer.formatColumnName(json['columns'][1]['name']);
+                    var col3 = ChatDrawer.formatColumnName(json['columns'][2]['name']);
+
+
+                    ChatDrawer.createBubbleChart(component, labelsX, labelsY, values, col1, col2, col3);
+                    ChatDrawer.refreshToolbarButtons(component, 'bubble');
+                }
+                if(e.target.classList.contains('export_png')){
+                    if(e.target.tagName == 'svg'){
+                        idRequest = e.target.parentElement.dataset.id;
+                    }else if(e.target.tagName == 'path'){
+                        idRequest = e.target.parentElement.parentElement.dataset.id;
+                    }else{
+                        idRequest = e.target.dataset.id;
+                    }
+                    var json = ChatDrawer.responses[idRequest];
+                    var component = document.querySelectorAll(`[data-componentid='${idRequest}']`)[0];
+                    var svg = component.getElementsByTagName('svg')[0];
+                    var svgString = ChatDrawer.getSVGString(svg);
+                    ChatDrawer.svgString2Image( svgString, 2*component.clientWidth, 2*component.clientHeight);
+                }
             }
         });
         chataInput.onkeyup = function(){
@@ -534,6 +573,180 @@ function copyTextToClipboard(text) {
         return values;
     }
 
+    ChatDrawer.createBubbleChart = function(component, labelsX, labelsY, data, col1, col2, col3){
+        var margin = {top: 5, right: 10, bottom: 50, left: 130},
+        width = component.parentElement.clientWidth - margin.left,
+        height = 600;
+        component.innerHTML = '';
+        component.parentElement.classList.remove('chata-table-container');
+        component.parentElement.classList.add('chata-chart-container');
+
+        var svg = d3.select(`[data-componentid='${component.dataset.componentid}']`)
+        .append("svg")
+        .attr("width", width + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
+        const barWidth = width / labelsX.length;
+        const barHeight = height / labelsY.length;
+
+        const intervalWidth = Math.ceil((labelsX.length * 16) / width);
+        const intervalHeight = Math.ceil((labelsY.length * 16) / height);
+
+        var xTickValues = [];
+        var yTickValues = [];
+        if (barWidth < 16) {
+            labelsX.forEach((element, index) => {
+                if (index % intervalWidth === 0) {
+                    if(element.length < 18){
+                        xTickValues.push(element);
+                    }else{
+                        xTickValues.push(element.slice(0, 18));
+                    }
+                }
+            });
+        }
+
+        if(barHeight < 16){
+            labelsY.forEach((element, index) => {
+                if (index % intervalHeight === 0) {
+                    if(element.length < 18){
+                        yTickValues.push(element);
+                    }else{
+                        yTickValues.push(element.slice(0, 18));
+                    }
+                }
+            });
+        }
+
+        var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return `
+            <span class='title-tip'>${col2}:</span> <span>${d.labelX}</span> <br/>
+            <span class='title-tip'>${col1}:</span> <span>${d.labelY}</span> <br/>
+            <span class='title-tip'>${col3}:</span> <span>${d.value}</span>`;
+        })
+
+        svg.call(tip);
+
+        svg.append('text')
+        .attr('x', -(height / 2))
+        .attr('y', -margin.left + margin.right)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .attr('class', 'y-axis-label')
+        .text(col1);
+
+        svg.append('text')
+        .attr('x', width / 2)
+        .attr('y', height + margin.bottom)
+        .attr('text-anchor', 'middle')
+        .attr('class', 'x-axis-label')
+        .text(col2);
+
+
+        var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(labelsX.map(function(d) {
+            if(d.length < 18){
+                return d;
+            }else{
+                return d.slice(0, 18);
+            }
+        }))
+        .padding(0.01);
+
+        var xAxis = d3.axisBottom(x);
+
+        if(xTickValues.length > 0){
+            xAxis.tickValues(xTickValues);
+        }
+
+        svg.append("g")
+        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .style("color", '#fff')
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+
+        var y = d3.scaleBand()
+        .range([ height - margin.bottom, 0])
+        .domain(labelsY.map(function(d) {
+            if(d.length < 18){
+                return d;
+            }else{
+                return d.slice(0, 18);
+            }
+        }))
+        .padding(0.01);
+
+        var yAxis = d3.axisLeft(y);
+
+        if(yTickValues.length > 0){
+            yAxis.tickValues(yTickValues);
+        }
+
+        svg.append("g")
+        .call(yAxis);
+
+        var radiusScale = d3.scaleLinear()
+        .range([0, 2 * Math.min(x.bandwidth(), y.bandwidth())])
+        .domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        svg.selectAll()
+        .data(data, function(d) {
+            var xLabel = '';
+            var yLabel = '';
+
+            if(d.labelX.length < 18){
+                xLabel = d.labelX;
+            }else{
+                xLabel = d.labelX.slice(0, 18);
+            }
+
+            if(d.labelY.length < 18){
+                yLabel = d.labelY;
+            }else{
+                yLabel = d.labelY.slice(0, 18);
+            }
+            return xLabel+':'+yLabel;
+        })
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) {
+            if(d.labelX.length < 18){
+                return x(d.labelX) + x.bandwidth() / 2;
+            }else{
+                return x(d.labelX.slice(0, 18)) + x.bandwidth() / 2;
+            }
+        })
+        .attr("cy", function (d) {
+            if(d.labelY.length < 18){
+                return y(d.labelY) + y.bandwidth() / 2;
+            }else{
+                return y(d.labelY.slice(0, 18)) + y.bandwidth() / 2;
+            }
+        })
+        .attr("r", function (d) { return d.value < 0 ? 0 : radiusScale(d.value); })
+        .attr("fill", "#28a8e0")
+        .attr("opacity", "0.7")
+        .attr('class', 'circle')
+        .on('mouseover', function(d) {
+            tip.attr('class', 'd3-tip animate').show(d)
+        })
+        .on('mouseout', function(d) {
+            tip.attr('class', 'd3-tip').show(d)
+            tip.hide()
+        });
+    }
+
     ChatDrawer.createHeatmap = function(component, labelsX, labelsY, data, col1, col2, col3){
         var margin = {top: 5, right: 10, bottom: 50, left: 130},
         width = component.parentElement.clientWidth - margin.left,
@@ -659,7 +872,7 @@ function copyTextToClipboard(text) {
 
         var colorScale = d3.scaleLinear()
         .range(["white", "#28a8e0"])
-        .domain([1, d3.max(data, function(d) { return d.value; })]);
+        .domain([0, d3.max(data, function(d) { return d.value; })]);
 
         svg.selectAll()
         .data(data, function(d) {
@@ -1153,7 +1366,10 @@ function copyTextToClipboard(text) {
     ChatDrawer.refreshToolbarButtons = function(oldComponent, activeDisplayType){
         var messageBubble = oldComponent.parentElement.parentElement.parentElement;
         var toolbarLeft = messageBubble.getElementsByClassName('left')[0];
+        var toolbarRight = messageBubble.getElementsByClassName('right')[0];
+        var actionType = ['table', 'pivot_column', 'date_pivot'].includes(activeDisplayType) ? 'csvCopy' : '';
         toolbarLeft.innerHTML = ChatDrawer.getSupportedDisplayTypes(oldComponent.dataset.componentid, activeDisplayType);
+        toolbarRight.innerHTML = ChatDrawer.getActionButtons(oldComponent.dataset.componentid, actionType);
     }
 
     ChatDrawer.createPivotTable = function(pivotArray, oldComponent){
@@ -1498,6 +1714,79 @@ function copyTextToClipboard(text) {
         ChatDrawer.drawerContent.scrollTop = ChatDrawer.drawerContent.scrollHeight;
     }
 
+    ChatDrawer.getSVGString = function( svgNode ) {
+        svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+
+        var serializer = new XMLSerializer();
+        var svgString = serializer.serializeToString(svgNode);
+        svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink=');
+        svgString = svgString.replace(/NS\d+:href/g, 'xlink:href');
+
+        return svgString;
+    }
+
+
+    ChatDrawer.svgString2Image = function(svgString, width, height) {
+        var imgsrc = 'data:image/svg+xml;base64,'+ btoa(unescape(encodeURIComponent(svgString)));
+
+        var canvas = document.createElement("canvas");
+        var context = canvas.getContext("2d");
+
+        canvas.width = width;
+        canvas.height = height;
+
+
+        var image = new Image();
+        image.onload = function() {
+            context.clearRect ( 0, 0, width, height );
+            context.drawImage(image, 0, 0, width, height);
+            var imgPixels = context.getImageData(0, 0, canvas.width, canvas.height);
+            for(var y = 0; y < imgPixels.height; y++){
+                for(var x = 0; x < imgPixels.width; x++){
+                    var i = (y * 4) * imgPixels.width + x * 4;
+                    imgPixels.data[i] = 0;
+                    imgPixels.data[i + 1] = 0;
+                    imgPixels.data[i + 2] = 0;
+                }
+            }
+            context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+
+            var link = document.createElement("a");
+            link.setAttribute('href', canvas.toDataURL("image/png;base64"));
+            link.setAttribute('download', 'Chart.png');
+            link.click();
+        };
+
+
+        image.src = imgsrc;
+    }
+
+    ChatDrawer.getActionButtons = function(idRequest, type){
+        if (type == 'csvCopy'){
+            return `
+            <button class="chata-toolbar-btn clipboard" data-id="${idRequest}">
+                <svg stroke="currentColor" class="clipboard" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path class="clipboard" d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z">
+                    </path>
+                </svg>
+            </button>
+            <button class="chata-toolbar-btn csv" data-id="${idRequest}">
+                <svg stroke="currentColor" class="csv" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" class="csv"></path>
+                </svg>
+            </button>
+            `;
+        }else{
+            return `
+            <button class="chata-toolbar-btn export_png" data-id="${idRequest}">
+                <svg stroke="currentColor" class="export_png" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" class="export_png"></path>
+                </svg>
+            </button>
+            `;
+        }
+    }
+
     ChatDrawer.getSupportedDisplayTypes = function(idRequest, ignore){
         var json = ChatDrawer.responses[idRequest];
         var buttons = '';
@@ -1619,12 +1908,12 @@ function copyTextToClipboard(text) {
 
     ChatDrawer.getBubbleChartButton = function(idRequest){
         return `
-            <button class="chata-toolbar-btn" data-tip="Bubble Chart" data-id="${idRequest}">
-                <svg x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" stroke="currentColor" fill="currentColor" stroke-width="0" height="1em" width="1em">
-                    <circle class="chart-icon-svg-0" cx="7.7" cy="11.1" r="1.2"></circle>
-                    <circle class="chart-icon-svg-0" cx="2.6" cy="8.8" r="2.6"></circle>
-                    <circle class="chart-icon-svg-0" cx="11.7" cy="4.3" r="4.3"></circle>
-                    <circle class="chart-icon-svg-0" cx="1.8" cy="14.8" r="1.2"></circle>
+            <button class="chata-toolbar-btn bubble_chart" data-tip="Bubble Chart" data-id="${idRequest}">
+                <svg class="bubble_chart" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" stroke="currentColor" fill="currentColor" stroke-width="0" height="1em" width="1em">
+                    <circle class="chart-icon-svg-0 bubble_chart" cx="7.7" cy="11.1" r="1.2"></circle>
+                    <circle class="chart-icon-svg-0 bubble_chart" cx="2.6" cy="8.8" r="2.6"></circle>
+                    <circle class="chart-icon-svg-0 bubble_chart" cx="11.7" cy="4.3" r="4.3"></circle>
+                    <circle class="chart-icon-svg-0 bubble_chart" cx="1.8" cy="14.8" r="1.2"></circle>
                 </svg>
             </button>
         `;
@@ -1649,6 +1938,7 @@ function copyTextToClipboard(text) {
         var idRequest = uuidv4();
         ChatDrawer.responses[idRequest] = jsonResponse;
         var supportedDisplayTypes = ChatDrawer.getSupportedDisplayTypes(idRequest, 'table');
+        var actions = ChatDrawer.getActionButtons(idRequest, 'csvCopy');
         var toolbar = '';
         if(supportedDisplayTypes != ''){
             toolbar += `
@@ -1659,17 +1949,7 @@ function copyTextToClipboard(text) {
         }
         toolbar += `
             <div class="chat-message-toolbar right">
-                <button class="chata-toolbar-btn clipboard" data-id="${idRequest}">
-                    <svg stroke="currentColor" class="clipboard" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                        <path class="clipboard" d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z">
-                        </path>
-                    </svg>
-                </button>
-                <button class="chata-toolbar-btn csv" data-id="${idRequest}">
-                    <svg stroke="currentColor" class="csv" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" class="csv"></path>
-                    </svg>
-                </button>
+                ${actions}
             </div>`;
 
         messageBubble.innerHTML = toolbar;
