d19a5594c8bfb9882953af4867900f198c06b4fd
oscar-ivan-rr <oscarivanramirezruiz96@gmail.com>
2020-01-22 22:00:08 -0600

    Notification settings modal front-end part 2

--- src/SettingsItem.js
+++ src/SettingsItem.js
@@ -1,7 +1,10 @@
-function SettingsItem(title){
+function SettingsItem(title, message){
     var wrapper = document.createElement('div');
     var header = document.createElement('div');
-    var displayName = document.createElement('div');
+    var settingsDisplayName = document.createElement('div');
+
+    var displayName = document.createElement('span');
+    var displayNameMessage = document.createElement('span');
     var settingsActions = document.createElement('div');
     var chataCheckbox = htmlToElement(`
         <div class="chata-checkbox">
@@ -14,20 +17,30 @@ function SettingsItem(title){
 
     wrapper.classList.add('chata-notification-setting-item');
     header.classList.add('chata-notification-setting-item-header');
-    displayName.classList.add('chata-notification-setting-display-name');
+    settingsDisplayName.classList.add('chata-notification-setting-display-name');
+    displayName.classList.add('chata-notification-setting-display-name-title');
+    displayNameMessage.classList.add('chata-notification-setting-display-name-message');
     settingsActions.classList.add('chata-notification-setting-actions')
     displayName.innerHTML = title;
+    displayNameMessage.innerHTML = message;
+
 
     settingsActions.appendChild(chataCheckbox)
-    header.appendChild(displayName);
+    settingsDisplayName.appendChild(displayName);
+    settingsDisplayName.appendChild(displayNameMessage);
+    header.appendChild(settingsDisplayName);
     header.appendChild(settingsActions);
     header.onclick = function(evt){
-        var configModal = new Modal({
-            withFooter: true,
-            destroyOnClose: true
-        })
-        configModal.setTitle('Custom Notification');
-        configModal.show();
+        var target = evt.target;
+        if(!target.classList.contains('chata-slider')
+            && target.tagName !== 'INPUT'){
+            var configModal = new Modal({
+                withFooter: true,
+                destroyOnClose: true
+            })
+            configModal.setTitle('Custom Notification');
+            configModal.show();
+        }
     }
     wrapper.appendChild(header);
     return wrapper;
