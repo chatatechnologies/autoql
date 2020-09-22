a377e584d3458299e0003790282f41ebbdb8b442
Oscar Ramirez <oscar@rinro.com.mx>
2020-09-21 22:09:29 -0500

    Added AutoQL Options section and UI Config section to react test page

--- test_page/src/DataMessengerPage/DataMessengerPage.js
+++ test_page/src/DataMessengerPage/DataMessengerPage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { AuthenticationForm } from '../AuthenticationForm'
 import {
     Switch,
-    Button
+    Button,
+    Radio
 } from 'antd'
 import {
     ReloadOutlined,
@@ -13,10 +14,66 @@ import './DataMessengerPage.css'
 export class DataMessengerPage extends Component {
 
     state = {
-        uiOverlay: false
+        uiOverlay: false,
+        isVisible: false,
+        placement: 'right',
+        showHandle: true,
+        theme: 'light',
+        response: null,
+        showMask: true,
+        shiftScreen: false,
+        userDisplayName: 'Oscar',
+        introMessage: undefined,
+        enableAutocomplete: true,
+        enableQueryValidation: true,
+        enableQuerySuggestions: true,
+        enableDrilldowns: true,
+        enableExploreQueriesTab: true,
+        enableNotificationsTab: true,
+        enableNotifications: true,
+        enableColumnVisibilityManager: true,
+        enableVoiceRecord: true,
+        dashboardTitleColor: 'rgb(72, 105, 142)',
+        clearOnClose: false,
+        height: 500,
+        width: 550,
+        title: 'Data Messenger',
+        lightAccentColor: '#26a7df',
+        // lightAccentColor: '#2466AE',
+        dashboardBackground: '#fafafa',
+        darkAccentColor: '#525252',
+        maxMessages: 12,
+        isEditing: false,
+        debug: true,
+        test: true,
+        currencyCode: 'USD',
+        languageCode: 'en-US',
+        currencyDecimals: undefined,
+        quantityDecimals: undefined,
+        fontFamily: 'sans-serif',
+        runDashboardAutomatically: false,
+        comparisonDisplay: true,
+        chartColors: ['#355C7D', '#6C5B7B', '#C06C84', '#f67280', '#F8B195'],
+        monthFormat: 'MMM YYYY',
+        dayFormat: 'll',
+        dashboardTiles: [],
+        activeDashboardId: undefined,
+        enableDynamicCharting: true,
+        defaultTab: 'data-messenger',
     }
 
-    createBooleanRadioGroup = (title, propName, propValues = []) => {
+    onChangeRadioGroup = (propName, e) => {
+        this.setState({ [propName]: e })
+    }
+
+    onChangeDMProp = (propName, e) => {
+        console.log(propName);
+        this.setState({ [propName]: e })
+    }
+
+    createBooleanRadioGroup = (
+        title, propName,
+        propValues = [], onChange=this.onChangeRadioGroup) => {
         return (
             <div>
                 <h4>{title}</h4>
@@ -24,9 +81,31 @@ export class DataMessengerPage extends Component {
                 defaultChecked={this.state[propName]}
                 checked={this.state[propName] === true}
                 onChange={(e) => {
-                    this.setState({ [propName]: e })
+                    onChange(propName, e)
                 }}
                 />
+            </div>
+        )
+    }
+
+    createRadioInputGroup = (title, propName, propValues = [], reload) => {
+        return (
+            <div>
+            <h4>{title}</h4>
+            {reload && <h6>(Must click 'Reload Data Messenger' to apply this)</h6>}
+            <Radio.Group
+            defaultValue={this.state[propName]}
+            onChange={(e) => this.setState({ [propName]: e.target.value })}
+            buttonStyle="solid"
+            >
+            {propValues.map((propValue) => {
+                return (
+                    <Radio.Button value={propValue} key={`${propName}-${propValue}`}>
+                    {propValue.toString()}
+                    </Radio.Button>
+                )
+            })}
+            </Radio.Group>
             </div>
         )
     }
@@ -53,6 +132,80 @@ export class DataMessengerPage extends Component {
                 icon={<MenuFoldOutlined />}>
                     Open Data Messenger
                 </Button>
+                <h2>AutoQL API Configuration Options</h2>
+                {this.createBooleanRadioGroup(
+                    'Enable Autocomplete',
+                    'enableAutocomplete',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Enable Query Validation',
+                    'enableQueryValidation',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Enable Query Suggestions',
+                    'enableQuerySuggestions',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup('Enable Drilldowns', 'enableDrilldowns', [
+                    true,
+                    false,
+                    this.onChangeDMProp
+                ])}
+                {this.createBooleanRadioGroup(
+                    'Enable Column Visibility Editor',
+                    'enableColumnVisibilityManager',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Enable Notifications',
+                    'enableNotifications',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Debug Mode - Show copy to SQL button in message toolbar',
+                    'debug',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                <h2>UI Configuration Options</h2>
+                {this.createBooleanRadioGroup(
+                    'Show Data Messenger Button',
+                    'showHandle',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Shift Screen on Open/Close',
+                    'shiftScreen',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createBooleanRadioGroup(
+                    'Darken Background Behind Data Messenger',
+                    'showMask',
+                    [true, false],
+                    this.onChangeDMProp
+                )}
+                {this.createRadioInputGroup('Theme', 'theme', ['light', 'dark'])}
+                {this.createRadioInputGroup('Data Messenger Placement', 'placement', [
+                    'top',
+                    'bottom',
+                    'left',
+                    'right',
+                ])}
+                {this.createRadioInputGroup(
+                    'Default Tab',
+                    'defaultTab',
+                    ['data-messenger', 'explore-queries'],
+                    true,
+                )}
             </div>
         )
     }
