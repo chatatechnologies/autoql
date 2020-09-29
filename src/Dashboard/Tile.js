6ce91477b78228464cd09eb615110cbd6f9fddd2
Oscar Ramirez <oscar@rinro.com.mx>
2020-09-28 22:44:50 -0500

    Publish version 0.0.4 to npm, update heroku app

--- src/Dashboard/Tile.js
+++ src/Dashboard/Tile.js
@@ -10,6 +10,7 @@ import {
     putLoadingContainer
 } from '../Utils'
 import { getGroupableFields } from '../Charts/ChataChartHelpers'
+import { createSafetynetContent, createSuggestionArray } from '../Safetynet'
 import Split from 'split.js'
 import { refreshTooltips } from '../Tooltips'
 import {
