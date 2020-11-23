01edd6e35cefcb33b3196f4da2aab8c180a542e1
Oscar Ramirez <oscar@rinro.com.mx>
2020-11-23 14:12:32 -0600

    Improved DrilldownModal styles, added split view to DrilldownModal

--- src/Dashboard/DrilldownModal/DrilldownModal.js
+++ src/Dashboard/DrilldownModal/DrilldownModal.js
@@ -1,13 +1,28 @@
 import {
     Modal
 } from '../../Modal'
+import Split from 'split.js'
 
 export function DrilldownModal(title, views=[]){
 
     var modal = new Modal({
         destroyOnClose: true
+    }, () => {
+        if(views.length > 1){
+            Split(views, {
+                direction: 'vertical',
+                sizes: [50, 50],
+                minSize: ['35%', '0'],
+                gutterSize: 7,
+                cursor: 'row-resize',
+                onDragEnd: () => {
+                    window.dispatchEvent(new CustomEvent('chata-resize', {}))
+                }
+            })
+        }
     })
-    modal.chataBody.classList.add('chata-modal-full-height');
+    modal.chataBody.classList.add('chata-modal-full-height')
+    modal.chataModal.style.width = '90vw'
     views.map(v => modal.addView(v))
     modal.setTitle(title)
 
