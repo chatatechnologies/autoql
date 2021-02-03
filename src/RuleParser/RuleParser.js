1a08b81ea3b287eb8ae0fb16317a101eec062341
Oscar Ramirez <oscar@rinro.com.mx>
2021-02-03 14:35:04 -0600

    remove code comments for deploy

--- src/RuleParser/RuleParser.js
+++ src/RuleParser/RuleParser.js
@@ -25,28 +25,5 @@ export function convert(rules, addTopOperator=true){
 			rule = []
 		}
 	}
-	// for (var i = 0; i < rules.length; i++) {
-	// 	var termValue = rules[i]['term_value'];
-	// 	var group = [];
-	// 	for (var x = 0; x < termValue.length; x++) {
-	// 		var term = termValue[x];
-	// 		var topOperator = term.condition;
-	// 		var rule = [];
-	// 		for (var j = 0; j < term.term_value.length; j++) {
-	// 			var cTerm = term.term_value[j];
-	// 			var operator = getOperator(cTerm.condition);
-	// 			rule.push(cTerm.term_value);
-	// 			if(operator)rule.push(operator);
-	// 		}
-	// 		if(topOperator !== 'TERMINATOR' && addTopOperator){
-	// 			rule.push(topOperator);
-	// 		}
-	// 		group.push(rule);
-	// 	}
-	// 	parsedRules.push(group)
-	// 	if(rules[i].condition !== 'TERMINATOR' && addTopOperator){
-	// 		group.push([rules[i].condition])
-	// 	}
-	// }
 	return parsedRules;
 }
