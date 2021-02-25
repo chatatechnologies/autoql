f032d118540a51f31e4f8d24cd119555d32ff77b
Oscar Ramirez <oscar@rinro.com.mx>
2021-02-25 13:15:47 -0600

    removed unused code from NotificationSettingsModal

--- src/Notifications/NotificationSettingsModal.js
+++ src/Notifications/NotificationSettingsModal.js
@@ -1,6 +1,5 @@
 import { htmlToElement, uuidv4, apiCall } from '../Utils'
 import {
-    ADD_GROUP,
     QUERY,
     NOTEBOOK,
     INPUT_BUBBLES,
@@ -30,17 +29,6 @@ export function NotificationSettingsModal(options, mode='create', rule={}){
     var loader = htmlToElement(`
         <div class="autoql-vanilla-spinner-loader hidden"></div>
     `)
-    var btnAddGroup = htmlToElement(`
-        <span>
-            <div class="autoql-vanilla-chata-btn default
-            notification-rule-add-btn-outer"
-                data-tippy-content="Add Condition Group">
-                <span data-test="chata-icon" class="chata-icon">
-                    ${ADD_GROUP}
-                </span>
-            </div>
-        </span>
-    `);
     wrapper.classList.add('chata-steps-container');
     var step1 = new ChataModalStep('Set up your Alert','1',);
     var step2 = new ChataModalStep('Select Alert Interval', '2');
@@ -49,8 +37,6 @@ export function NotificationSettingsModal(options, mode='create', rule={}){
         '3',
         'When this Alert is triggered:'
     );
-    // var step4 = new ChataModalStep('Appearance', '4');
-    // step4.classList.add('complete');
 
     // STEP 1
     var step1ButtonContainer = document.createElement('div');
@@ -480,19 +466,7 @@ export function NotificationSettingsModal(options, mode='create', rule={}){
     }
 
     const setFrequency = () => {
-        if(rule.notification_type === 'CONTINUOUS'){
-            frequencyValue.innerHTML = 'Every time this happens';
-        }else {
-            frequencyValue.innerHTML = 'Once, when this happens';
-            showFrequencyView(relativeDiv, 0);
-        }
-        frequencyValue.setAttribute(
-            'data-frequency-event', rule.notification_type
-        )
         step2.classList.add('complete');
-
-        frequencyValue.style.color = 'inherit';
-        frequencyValue.style.fontStyle = 'inherit';
     }
 
     if(mode === 'edit'){
@@ -607,173 +581,6 @@ function FrequencyBox(message){
     parent.appendChild(box);
 
     return parent;
-}
-
-function frequencyView(parentElement, popupValues, label, followText){
-    parentElement.innerHTML = '';
-    var repeatFollowText = htmlToElement(`
-        <span class="frequency-repeat-follow-text"> on:</span>
-    `);
-    var checkboxContainer = htmlToElement(`
-        <div class="chata-checkbox">
-            <div class="chata-checkbox-label">Repeat</div>
-        </div>
-    `);
-    var checkboxFrequency = htmlToElement(`
-        <div
-            style="display: inline-block; vertical-align: middle;">
-        </div>
-    `);
-    checkboxFrequency.appendChild(checkboxContainer);
-    parentElement.appendChild(checkboxFrequency);
-    parentElement.style.visibility = 'visible';
-    repeatFollowText.style.visibility='hidden';
-    var labelContent = htmlToElement(`
-        <div>${label}</div>
-    `)
-    var frequencyButton = htmlToElement(`
-        <div class="chata-select notification-frequency-select">
-
-        </div>
-    `);
-    var popupFrequencySelect = PopupContainer(popupValues);
-
-    frequencyButton.appendChild(labelContent);
-    frequencyButton.appendChild(popupFrequencySelect);
-    frequencyButton.style.display = 'inline-block';
-
-    // checkboxInput.onchange = (evt) => {
-    //     if(evt.target.checked){
-    //         frequencyButton.style.visibility = 'visible';
-    //         repeatFollowText.style.visibility='visible'
-    //         if(parentElement.dateSelectView){
-    //             parentElement.dateSelectView.style.visibility = 'visible';
-    //         }
-    //     }else{
-    //         frequencyButton.style.visibility = 'hidden';
-    //         repeatFollowText.style.visibility='hidden';
-    //         if(parentElement.dateSelectView){
-    //             parentElement.dateSelectView.style.visibility = 'hidden';
-    //         }
-    //     }
-    // }
-    frequencyButton.onclick = () => {
-        popupFrequencySelect.toggleVisibility();
-    }
-
-    parentElement.appendChild(frequencyButton);
-    if(followText){
-        // parentElement.appendChild(repeatFollowText);
-    }
-
-    popupFrequencySelect.convertValue = (text) => {
-        switch (text) {
-            case 'Monthly':
-                return 'MONTH';
-            case 'Daily':
-                return 'DAY';
-            case 'Weekly':
-                return 'WEEK';
-            default:
-
-        }
-    }
-
-    popupFrequencySelect.setValue = (text) => {
-        labelContent.textContent = text;
-        labelContent.setAttribute(
-            'data-frequency-value', popupFrequencySelect.convertValue(text)
-        )
-    }
-
-    popupFrequencySelect.hideFrequency = () => {
-        checkboxContainer.style.display = 'none';
-        frequencyButton.style.display = 'none';
-    }
-
-    popupFrequencySelect.showFrequency = () => {
-        checkboxContainer.style.display = 'inline-block';
-        frequencyButton.style.display = 'inline-block';
-    }
-
-    labelContent.setAttribute(
-        'data-frequency-value', popupFrequencySelect.convertValue(label)
-    )
-
-    return popupFrequencySelect;
-}
-
-function onFrequencyPopupClick(evt, frequencyElement, indexZero=false){
-    var index = parseInt(evt.target.dataset.indexOption);
-    if(indexZero)index += 1;
-    frequencyElement.popup.setValue(evt.target.textContent);
-    switch (index) {
-        case 1:
-            // var view = weeklyView();
-            // if(!frequencyElement.dateSelectView){
-            //     frequencyElement.appendChild(view);
-            // }else{
-            //     frequencyElement.replaceChild(
-            //         view, frequencyElement.dateSelectView
-            //     );
-            // }
-            // frequencyElement.dateSelectView = view;
-            break;
-        case 2:
-            // var view = monthlyView();
-            // if(!frequencyElement.dateSelectView){
-            //     frequencyElement.appendChild(view);
-            // }else{
-            //     frequencyElement.replaceChild(
-            //         view, frequencyElement.dateSelectView
-            //     );
-            // }
-            // frequencyElement.dateSelectView = view;
-            break;
-        case 3:
-            // var view = yearlyView();
-            // if(!frequencyElement.dateSelectView){
-            //     frequencyElement.appendChild(view);
-            // }else{
-            //     frequencyElement.replaceChild(
-            //         view, frequencyElement.dateSelectView
-            //     );
-            // }
-            // frequencyElement.dateSelectView = view;
-            break;
-        default:
-            // if(frequencyElement.dateSelectView){
-            //     frequencyElement.removeChild(
-            //         frequencyElement.dateSelectView
-            //     );
-            //     frequencyElement.dateSelectView = null;
-            // }
-            break;
-    }
-}
-
-function showFrequencyView(frequencyElement, type){
-    switch (type) {
-        case 0:
-            var popup = frequencyView(frequencyElement, [
-                {text: 'Daily', active:false},
-                {text: 'Weekly', active:false},
-                {text: 'Monthly', active:true},
-            ], 'Monthly', true);
-            frequencyElement.popup = popup;
-            popup.onclick = (evt) => {
-                onFrequencyPopupClick(evt, frequencyElement);
-            }
-            break;
-        case 1:
-            frequencyElement.innerHTML = '';
-            break;
-        case 2:
-            frequencyElement.innerHTML = '';
-            break;
-        default:
-            break;
-    }
 }
 
 function checkStep2(step2){
@@ -916,43 +723,6 @@ function notificationRuleAndOrSelect(text, onChange){
     return div;
 }
 
-function notificationAndOrBreak(isFirst, text=''){
-    var wrapper = document.createElement('div');
-    wrapper.notificationAndOrText = null;
-    wrapper.isFirst = isFirst;
-    wrapper.classList.add('notification-and-or-break');
-    wrapper.setColor = (text) => {
-        if(text === 'AND'){
-            andOrText.style.background = 'rgb(186, 233, 255)';
-            andOrText.style.border = '1px solid rgb(144, 221, 255)';
-        }else{
-            andOrText.style.background = 'rgb(255, 250, 202)';
-            andOrText.style.border = '1px solid rgb(255, 235, 59)';
-        }
-    }
-    wrapper.setText = (text) => {
-        if(wrapper.notificationAndOrText !== null){
-            wrapper.notificationAndOrText.innerHTML = text;
-            wrapper.setColor(text);
-        }
-    }
-    if(isFirst){
-        wrapper.style.top = '0px';
-        wrapper.style.height = '100%';
-        wrapper.style.visibility = 'hidden';
-    }else{
-        wrapper.style.top = '-19px';
-        wrapper.style.height = 'calc(100% + 19px)';
-        var andOrText = document.createElement('div');
-        andOrText.classList.add('notification-and-or-text');
-        andOrText.innerHTML = text;
-        wrapper.appendChild(andOrText);
-        wrapper.notificationAndOrText = andOrText;
-        wrapper.setColor(text);
-    }
-    return wrapper;
-}
-
 function PopupContainer(options=[]){
     var container = document.createElement('div');
     var ul = document.createElement('ul');
@@ -1019,11 +789,6 @@ function GroupLine(params, expression=[]){
         <span
             class="chata-icon chata-rule-delete-btn">
             ${INPUT_DELETE}
-        </span>
-    `);
-    var spanBubbleIcon = htmlToElement(`
-        <span class="chata-icon rule-input-select-bubbles-icon">
-            ${INPUT_BUBBLES}
         </span>
     `);
 
@@ -1256,24 +1021,16 @@ function GroupLine(params, expression=[]){
     inputContainer2.appendChild(termError2);
 
     ruleContainer.classList.add('chata-notification-rule-container');
-    chataSelectTermType.classList.add('chata-select');
-    chataSelectTermType.classList.add('chata-rule-term-type-selector');
     secondContainer.classList.add('chata-rule-second-input-container');
     secondContainer.appendChild(inputContainer2);
     secondContainer.appendChild(termError2);
 
-    // secondContainer.appendChild(chataSelectTermType);
 
     chataSelect.classList.add('chata-select');
     chataSelect.classList.add('chata-rule-condition-select');
     chataSelect.appendChild(popup);
     chataSelect.appendChild(conditionValueSelect);
     chataSelect.conditionElement = conditionValueSelect;
-    chataSelectTermType.classList.add('chata-select');
-    chataSelectTermType.classList.add('chata-rule-term-type-selector');
-    chataSelectTermType.appendChild(spanBubbleIcon);
-    chataSelectTermType.appendChild(popupQuery);
-
 
     ruleContainer.inputContainer1 = inputContainer1;
     ruleContainer.chataSelect = chataSelect;
@@ -1302,196 +1059,6 @@ function GroupLine(params, expression=[]){
     }
 
     return ruleContainer;
-}
-
-function ConditionGroup(parentOptions, step1, parent, parentSelect, first=false, ruleLines={}){
-    var groupWrapper = document.createElement('div');
-    var groupContainer = document.createElement('div');
-    var ruleContainer = document.createElement('div');
-    var chataSelect = document.createElement('div');
-    var secondContainer = document.createElement('div');
-    var chataSelectTermType = document.createElement('div');
-    var uuid = uuidv4();
-    var groupValues = {
-        id: uuid,
-        term_type: 'group',
-        condition: 'AND'
-    }
-    if(ruleLines.id){
-        groupValues.id = ruleLines.id
-        groupValues.term_type = ruleLines.term_type
-        groupValues.condition = ruleLines.condition
-    }
-    this.groupLines = [];
-    var obj = this;
-    var onChangeAndOr = (evt) => {
-        if(evt.target.textContent === 'ALL')groupValues.condition = 'AND';
-        else groupValues.condition = 'OR';
-    }
-
-    var onDeleteLine = (evt, elem) => {
-        let index;
-        var uuid = elem.dataset.uuid;
-        for (var i = 0; i < obj.groupLines.length; i++) {
-            if(obj.groupLines[i].dataset.uuid === uuid){
-                index = i;
-                break;
-            }
-        }
-        obj.groupLines.splice(index, 1);
-        groupContainer.removeChild(elem);
-        checkStep1(parent);
-        step1.stepContentContainer.style.height = getHeightForChildrens(
-            step1.stepContentContainer
-        ) + 'px';
-    }
-
-    var onSelectRule = () => {
-        checkStep1(parent);
-    }
-
-    var ruleAndOrSelect = notificationRuleAndOrSelect(
-        ' of the following conditions are met:', onChangeAndOr
-    );
-    if(groupValues.condition === 'OR'){
-        ruleAndOrSelect.radio.toggleButtons('OR');
-    }
-    var notificationGroupDeleteBtn = htmlToElement(`
-        <div
-            class="chata-notification-group-delete-btn"
-            <span data-test="chata-icon" class="chata-icon">
-                ${INPUT_DELETE}
-            </span>
-        </div>
-    `);
-
-    var notificationRuleAddBtn = document.createElement('div');
-    notificationRuleAddBtn.classList.add('notification-rule-btn-container');
-
-    var addRuleButton = htmlToElement(`
-        <div class="chata-notification-rule-add-btn">
-            <span class="chata-icon chata-notification-add-icon">
-                ${ADD_GROUP}
-            </span>
-        </div>
-    `)
-    notificationRuleAddBtn.appendChild(addRuleButton);
-    groupWrapper.notificationRuleAddBtn = notificationRuleAddBtn;
-    groupWrapper.classList.add('notification-group-wrapper');
-    let andOrBreak = notificationAndOrBreak(first, parentSelect.operator);
-
-    notificationGroupDeleteBtn.onclick = function(){
-        parent.removeChild(groupWrapper);
-        var groups = document.getElementsByClassName(
-            'notification-group-wrapper'
-        );
-        if(groups.length >= 1){
-            groups[0].setAsFirtsAndOrBreak();
-        }
-        if(groups.length > 1){
-            groups[0].showNotificationAndOrBreak();
-        }
-        showLeftAndOr(parentSelect, parent);
-        checkStep1(parent);
-        step1.stepContentContainer.style.height = getHeightForChildrens(
-            step1.stepContentContainer
-        ) + 'px';
-    }
-
-    addRuleButton.onclick = function(){
-        var newGroupLine = new GroupLine({
-            onDeleteLine: onDeleteLine,
-            onSelectRule: onSelectRule,
-            parentOptions: parentOptions,
-            step: step1
-        });
-        obj.groupLines.push(newGroupLine);
-        groupContainer.insertBefore(newGroupLine, notificationRuleAddBtn);
-        checkStep1(parent);
-        step1.stepContentContainer.style.height = getHeightForChildrens(
-            step1.stepContentContainer
-        ) + 'px';
-    }
-
-    groupWrapper.setAsFirtsAndOrBreak = function(){
-        var newAndOrBreak = notificationAndOrBreak(
-            true, parentSelect.operator
-        );
-        groupWrapper.replaceChild(
-            newAndOrBreak, groupWrapper.notificationAndOrBreak
-        );
-        groupWrapper.notificationAndOrBreak = newAndOrBreak;
-    }
-
-    groupWrapper.isValid = function(){
-        var lines = obj.groupLines;
-        if(lines.length === 0){
-            return false;
-        }
-        for (var i = 0; i < lines.length; i++) {
-            if(lines[i].isEmpty()){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    groupWrapper.notificationAndOrBreak = andOrBreak;
-    groupWrapper.appendChild(andOrBreak);
-    groupWrapper.hideNotificationAndOrBreak = function(){
-        groupWrapper.notificationAndOrBreak.style.visibility = 'hidden';
-    }
-    groupWrapper.showNotificationAndOrBreak = function(){
-        groupWrapper.notificationAndOrBreak.style.visibility = 'visible';
-    }
-    groupWrapper.style.marginLeft = '0px';
-    groupContainer.classList.add('chata-notification-group-container-copy');
-    groupContainer.classList.add('disable-first-delete');
-    ruleContainer.classList.add('chata-notification-rule-container');
-    chataSelect.classList.add('chata-select');
-    chataSelect.classList.add('chata-rule-condition-select');
-    chataSelectTermType.classList.add('chata-select');
-    chataSelectTermType.classList.add('chata-rule-term-type-selector');
-
-    chataSelect.innerHTML = '&gt;';
-    secondContainer.classList.add('chata-rule-second-input-container');
-    if(ruleLines.parsedLines){
-        ruleLines.parsedLines.map((line) => {
-            var groupLine = new GroupLine({
-                onDeleteLine: onDeleteLine,
-                onSelectRule: onSelectRule,
-                parentOptions: parentOptions,
-                step: step1
-            }, line)
-
-            obj.groupLines.push(groupLine)
-            groupContainer.appendChild(groupLine);
-        })
-    }else{
-        var defaultGroup = new GroupLine({
-            onDeleteLine: onDeleteLine,
-            onSelectRule: onSelectRule,
-            parentOptions: parentOptions,
-            step: step1
-        });
-        obj.groupLines.push(defaultGroup);
-        groupContainer.appendChild(defaultGroup);
-    }
-
-    groupWrapper.getLines = () => {
-        return obj.groupLines;
-    }
-
-    groupWrapper.getValues = () => {
-        return groupValues;
-    }
-
-    groupContainer.appendChild(ruleAndOrSelect);
-    groupContainer.appendChild(notificationGroupDeleteBtn);
-    groupContainer.appendChild(notificationRuleAddBtn);
-    groupWrapper.appendChild(groupContainer);
-
-    return groupWrapper;
 }
 
 function getHeightForChildrens(parent){
