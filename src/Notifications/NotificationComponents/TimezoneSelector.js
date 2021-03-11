e6c3f2218849c0512f93cc71c2fc4545b8e1710f
Oscar Ramirez <oscar@rinro.com.mx>
2021-03-11 16:12:47 -0600

    Added styles to TimezoneSelector

--- src/Notifications/NotificationComponents/TimezoneSelector.js
+++ src/Notifications/NotificationComponents/TimezoneSelector.js
@@ -3,11 +3,19 @@ export function TimezoneSelector(){
     var wrapper = document.createElement('div')
     var text = document.createElement('span')
     var selectControl = document.createElement('div')
+    var selectWithArrow = document.createElement('div')
+    var valueContainer = document.createElement('div')
+    var valueElement = document.createElement('div')
     text.textContent = 'Time zone: '
 
     obj.classList.add('autoql-vanilla-schedule-builder-timezone-section')
     selectControl.classList.add('autoql-vanilla-timezone-select')
-    selectControl.textContent = 'TEST'
+    selectWithArrow.classList.add('autoql-vanilla-select-with-arrow')
+    valueElement.textContent = 'TEST'
+
+    valueContainer.appendChild(valueElement)
+    selectWithArrow.appendChild(valueContainer)
+    selectControl.appendChild(selectWithArrow)
     wrapper.appendChild(text)
     wrapper.appendChild(selectControl)
     obj.appendChild(wrapper)
