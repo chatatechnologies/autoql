b1ba96ab1ffb7d9c19bfa5a5493236866723d745
Oscar Ramirez <oscar@rinro.com.mx>
2021-03-12 15:04:41 -0600

    Added controls to TimezoneSelect

--- src/Notifications/NotificationComponents/TimezoneSelector.js
+++ src/Notifications/NotificationComponents/TimezoneSelector.js
@@ -6,15 +6,23 @@ export function TimezoneSelector(){
     var selectWithArrow = document.createElement('div')
     var valueContainer = document.createElement('div')
     var valueElement = document.createElement('div')
+    var indicators = document.createElement('div')
+    var indicatorSeparator = document.createElement('div')
     text.textContent = 'Time zone: '
 
     obj.classList.add('autoql-vanilla-schedule-builder-timezone-section')
     selectControl.classList.add('autoql-vanilla-timezone-select')
     selectWithArrow.classList.add('autoql-vanilla-select-with-arrow')
+    valueContainer.classList.add('autoql-vanilla-select-value-container')
+    indicators.classList.add('autoql-vanilla-select-indicators')
+    indicatorSeparator.classList.add('autoql-vanilla-indicator-separator')
+
     valueElement.textContent = 'TEST'
 
+    indicators.appendChild(indicatorSeparator)
     valueContainer.appendChild(valueElement)
     selectWithArrow.appendChild(valueContainer)
+    selectWithArrow.appendChild(indicators)
     selectControl.appendChild(selectWithArrow)
     wrapper.appendChild(text)
     wrapper.appendChild(selectControl)
