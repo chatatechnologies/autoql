689c0c9932c714e0664dba51b2e30ce0dd42d3a4
Oscar Ramirez <oscar@rinro.com.mx>
2022-02-21 17:12:30 -0600

    Added interpretation text

--- src/ReverseTranslation/ReverseTranslation.js
+++ src/ReverseTranslation/ReverseTranslation.js
@@ -1,12 +1,16 @@
 import './ReverseTranslation.css'
 
 export function ReverseTranslation(interpretation){
+    console.log(interpretation);
     var container = document.createElement('div')
     var label = document.createElement('strong')
+    var textContainer = document.createElement('span')
     label.textContent = 'Interpreted as: '
+    textContainer.textContent = interpretation
 
     container.classList.add('autoql-vanilla-reverse-translation-container')
     container.appendChild(label)
+    container.appendChild(textContainer)
 
     return container
 }
