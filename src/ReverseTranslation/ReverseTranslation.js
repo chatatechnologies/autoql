37797ff1af3c063d0339ee9746fd363c97850328
Oscar Ramirez <oscar@rinro.com.mx>
2022-02-22 00:14:25 -0600

    Parser interpretation values

--- src/ReverseTranslation/ReverseTranslation.js
+++ src/ReverseTranslation/ReverseTranslation.js
@@ -3,13 +3,20 @@ import { INFO_ICON } from '../Svg'
 
 export function ReverseTranslation(interpretation){
     console.log(interpretation);
+    console.log();
+
+    const reverseTranslation = interpretation.replace(/(["'])(?:(?=(\\?))\2.)*?\1/gi, (output) => {
+        const text = output.replace(/'/g, '')
+        console.log(text);
+        return `<a class="autoql-vanilla-condition-link">${text}</a>`
+    })
     var container = document.createElement('div')
     var label = document.createElement('strong')
     var textContainer = document.createElement('span')
     var iconContainer = document.createElement('span')
 
     label.textContent = 'Interpreted as: '
-    textContainer.textContent = interpretation
+    textContainer.innerHTML = reverseTranslation
     iconContainer.innerHTML = INFO_ICON
 
     container.classList.add('autoql-vanilla-reverse-translation-container')
