f5979344282e323ff693b34b95d2e438774e5d2e
Oscar Ramirez <oscar@rinro.com.mx>
2022-02-21 23:27:16 -0600

    Added info icon to interpretation

--- src/ReverseTranslation/ReverseTranslation.js
+++ src/ReverseTranslation/ReverseTranslation.js
@@ -1,14 +1,21 @@
 import './ReverseTranslation.css'
+import { INFO_ICON } from '../Svg'
 
 export function ReverseTranslation(interpretation){
     console.log(interpretation);
     var container = document.createElement('div')
     var label = document.createElement('strong')
     var textContainer = document.createElement('span')
+    var iconContainer = document.createElement('span')
+
     label.textContent = 'Interpreted as: '
     textContainer.textContent = interpretation
+    iconContainer.innerHTML = INFO_ICON
 
     container.classList.add('autoql-vanilla-reverse-translation-container')
+    iconContainer.classList.add('autoql-vanilla-reverse-translation-icon')
+
+    container.appendChild(iconContainer)
     container.appendChild(label)
     container.appendChild(textContainer)
 
