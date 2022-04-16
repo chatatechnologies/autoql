aec1645d327c3b52f3f0f91a0d2be679f9b8045d
Oscar Ramirez <oscar@rinro.com.mx>
2022-04-15 22:33:58 -0500

    Added header to FilterLocking

--- src/FilterLocking/FilterLocking.js
+++ src/FilterLocking/FilterLocking.js
@@ -1,9 +1,27 @@
 import './FilterLocking.css'
-
+import { INFO_ICON } from '../Svg'
 export function FilterLocking(){
     var view = document.createElement('div')
+    var header = document.createElement('div')
+    var titleContainer = document.createElement('div')
+    var title = document.createElement('h3')
+    var infoIcon = document.createElement('span')
+
     view.classList.add('autoql-vanilla-filter-locking-view')
     view.classList.add('autoql-vanilla-popover-container')
+    header.classList.add('autoql-vanilla-condition-lock-header')
+    titleContainer.classList.add('autoql-vanilla-filter-locking-title-container')
+    title.classList.add('autoql-vanilla-filter-locking-title')
+    // infoIcon.classList.add('autoql-vanilla-chata-icon')
+
+    title.textContent = 'Filter Locking'
+    infoIcon.innerHTML = INFO_ICON
+
+    title.appendChild(infoIcon)
+    titleContainer.appendChild(title)
+    header.appendChild(titleContainer)
+
+    view.appendChild(header)
 
     view.isOpen = false
 
