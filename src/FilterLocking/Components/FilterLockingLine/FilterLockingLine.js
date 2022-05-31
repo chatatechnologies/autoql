c4ae820e5c6e89e8baa33aa18800c216b3f0d931
Oscar Ramirez <oscar@rinro.com.mx>
2022-05-31 01:23:12 -0500

    added title to FilterLockingLine

--- src/FilterLocking/Components/FilterLockingLine/FilterLockingLine.js
+++ src/FilterLocking/Components/FilterLockingLine/FilterLockingLine.js
@@ -1,6 +1,38 @@
+import './FilterLockingLine.css'
+
 export function FilterLockingLine(data){
+    const {
+        show_message
+    } = data[0]
     console.log(data);
+    // app_id: 1500
+    // customer_id: "accounting-demo"
+    // filter_type: "include"
+    // id: 10298
+    // key: "ITEM_0_NAME_VALUE_LABEL"
+    // lock_flag: null
+    // model_id: 10010
+    // project_id: 159
+    // show_message: "Item Name"
+    // user_id: "vidhya@chata.ai"
+    // user_type: "user"
+    // value: "Seals"
     var view = document.createElement('div')
-    view.innerHTML = data[0].show_message
+    var titleContainer = document.createElement('div')
+    var titleWrapper = document.createElement('div')
+    var title = document.createElement('h4')
+    var categoryContainer = document.createElement('span')
+
+    categoryContainer.textContent = show_message
+
+    categoryContainer.classList.add('autoql-vanilla-filter-lock-category-title')
+    titleContainer.classList.add('autoql-vanilla-filter-list-title')
+
+    title.appendChild(categoryContainer)
+    titleWrapper.appendChild(title)
+    titleContainer.appendChild(titleWrapper)
+    view.appendChild(titleContainer)
+
+    view.data = data
     return view
 }
