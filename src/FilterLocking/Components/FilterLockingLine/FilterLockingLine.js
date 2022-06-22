a4a24d8068472b1db434899a4c97d8402a0280a0
Oscar Ramirez <oscar@rinro.com.mx>
2022-06-21 23:01:10 -0500

    improved styles for FilterLocking

--- src/FilterLocking/Components/FilterLockingLine/FilterLockingLine.js
+++ src/FilterLocking/Components/FilterLockingLine/FilterLockingLine.js
@@ -10,16 +10,19 @@ export function FilterLockingLine(conditionData){
     var label = document.createElement('div')
     var settings = document.createElement('div')
     var removeButton = document.createElement('span')
+    var sliderWrapper = document.createElement('div')
     var slider = new ChataSlider()
     view.classList.add('autoql-vanilla-filter-locking-line')
     label.classList.add('autoql-vanilla-condition-table-list-item')
     settings.classList.add('autoql-vanilla-condition-table-settings')
     removeButton.classList.add('autoql-vanilla-remove-condition-button')
+    sliderWrapper.classList.add('autoql-vanilla-slider-wrapper')
 
     label.textContent = value
     removeButton.innerHTML = CLEAR_ALL
 
-    settings.appendChild(slider)
+    sliderWrapper.appendChild(slider)
+    settings.appendChild(sliderWrapper)
     settings.appendChild(removeButton)
     view.appendChild(label)
     view.appendChild(settings)
