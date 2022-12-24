71bba48794f2b4f38c0dc43810bcb691c8c374a3
Oscar Ramirez <oscar@rinro.com.mx>
2022-12-24 00:31:39 -0600

    added DisableOutputWebpackPluginDisableO

--- extract-css.config.js
+++ extract-css.config.js
@@ -1,15 +1,17 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const DisableOutputWebpackPlugin = require('disable-output-webpack-plugin');
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin({
-      filename: 'autoql.min.css'
-  })],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'autoql.min.css' }),
+        new DisableOutputWebpackPlugin({ }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
 };
