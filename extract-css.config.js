99c8e7a5998b49ed50a402a0331fc516a3f7f67f
Nikki Moore <nmoore@chata.ai>
2023-06-19 08:57:11 -0600

    update webpack and eslint settings

--- extract-css.config.js
+++ extract-css.config.js
@@ -1,15 +1,13 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    plugins: [
-        new MiniCssExtractPlugin({ filename: 'autoql.min.css' }),
+  plugins: [new MiniCssExtractPlugin({ filename: "autoql.min.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
-    },
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,40 @@
-const path = require('path')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'autoql-min.js',
-        path: path.resolve(__dirname, 'build'),
-        libraryTarget: 'umd',
-        globalObject: 'this'
-    },
-    module: {
-        rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            {test: /\.css$/i, use: ['style-loader', 'css-loader']}
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    optimization: {
-        minimize: true
-    },
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "autoql-min.js",
+    path: path.resolve(__dirname, "build"),
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /test_page/, /test/],
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "autoql.min.css" })],
+  resolve: {
+    extensions: [".js", ".jsx", ".css", ".scss"],
+  },
+  externals: {
+    // Exclude all modules from node_modules
+    "/.*/": "umd $0",
+  },
+  optimization: {
+    minimize: true,
+  },
+};
