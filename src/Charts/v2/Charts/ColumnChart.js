1fc5fc06cf09b7caff18f4f0356c75d20d3ca3c3
Oscar Ramirez <oscar@rinro.com.mx>
2023-06-21 16:01:39 -0600

    added scales for ColumnChart

--- src/Charts/v2/Charts/ColumnChart.js
+++ src/Charts/v2/Charts/ColumnChart.js
@@ -1,4 +1,29 @@
+import {
+  CHART_MARGINS
+} from '../../../Constants';
+import {
+  SCALE_BAND,
+  SCALE_LINEAR,
+  getBandWidth
+} from '../../d3-compatibility';
+
 export function ColumnChart(widgetOptions, options) {
   console.log(widgetOptions);
   console.log(options);
+  const {
+    width,
+    height,
+    labelsNames,
+    minMaxValues,
+    groupNames,
+  } = options
+
+  const x0 = SCALE_BAND();
+  const x1 = SCALE_BAND();
+  const y = SCALE_LINEAR();
+
+  setDomainRange(x0, labelsNames, 0, width, false, .1)
+  const x1Range = minMaxValues.max === 0 ? 0 : getBandWidth(x0)
+  setDomainRange(x1, groupNames, 0, x1Range, false, .1)
+
 }
