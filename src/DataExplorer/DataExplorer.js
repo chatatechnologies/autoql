f640b038405c823e5125d2dad1a338539718de99
Oscar Ramirez <oscar@rinro.com.mx>
2023-07-05 21:06:44 -0600

    added title to intro message

--- src/DataExplorer/DataExplorer.js
+++ src/DataExplorer/DataExplorer.js
@@ -1,14 +1,16 @@
-import { SEARCH_ICON } from "../Svg";
+import { SEARCH_ICON, DATA_EXPLORER_SEARCH_ICON } from "../Svg";
 import { htmlToElement } from "../Utils";
 import { strings } from "../Strings";
 
 export function DataExplorer() {
   let obj = this;
   const searchIcon = htmlToElement(SEARCH_ICON);
-  var container = document.createElement('div');
-  var textBar = document.createElement('div');
-  var input = document.createElement('input');
-  var chatBarInputIcon  = document.createElement('div');
+  const container = document.createElement('div');
+  const textBar = document.createElement('div');
+  const input = document.createElement('input');
+  const chatBarInputIcon  = document.createElement('div');
+  const introMessage = document.createElement('div');
+  const title = document.createElement('h2');
 
   textBar.classList.add('autoql-vanilla-text-bar');
   textBar.classList.add('autoql-vanilla-text-bar-animation');
@@ -18,12 +20,18 @@ export function DataExplorer() {
   input.classList.add('autoql-vanilla-chata-input');
   input.classList.add('autoql-vanilla-explore-queries-input');
   input.classList.add('left-padding');
+  introMessage.classList.add('autoql-vanilla-data-explorer-intro-message');
   input.setAttribute('placeholder', strings.exploreQueriesInput);
-
+  title.appendChild(document.createTextNode('Welcome to '));
+  title.appendChild(htmlToElement(DATA_EXPLORER_SEARCH_ICON));
+  title.appendChild(document.createTextNode('Data Explorer'));
+  
   chatBarInputIcon.appendChild(searchIcon);
   textBar.appendChild(input);
   textBar.appendChild(chatBarInputIcon);
+  introMessage.appendChild(title);
   container.appendChild(textBar);
+  container.appendChild(introMessage);
   container.style.display = 'none';
   
   input.addEventListener('keydown', async event => {
@@ -42,5 +50,4 @@ export function DataExplorer() {
   }
 
   obj.container = container;
-
 }
