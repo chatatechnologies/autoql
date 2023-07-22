1f5561c4bf02df5a119a27d3d15da99746d8dd86
Oscar Ramirez <oscar@rinro.com.mx>
2023-07-22 16:09:12 -0600

    added values to DataAlertItem

--- src/Notifications/DataAlerts/Components/DataAlertItem.js
+++ src/Notifications/DataAlerts/Components/DataAlertItem.js
@@ -5,5 +5,37 @@ export function DataAlertItem({ dataAlert }) {
   item.classList.add('autoql-vanilla-notification-setting-item');
   row.classList.add('autoql-vanilla-notification-setting-item-header');
 
+  const {
+    title,
+    schedules,
+    status,
+  } = dataAlert;
+
+  const createCol = (className, text) => {
+    const section = document.createElement('div');
+    const content = document.createElement('div');
+    const wrapperContent = document.createElement('span');
+    const value = document.createElement('span');
+
+    section.classList.add('autoql-vanilla-data-alert-list-item-section');
+    content.classList.add('autoql-vanilla-data-alert-section-content');
+    wrapperContent.classList.add('autoql-vanilla-notification-setting-display-name-title');
+    value.textContent = text;
+
+    wrapperContent.appendChild(value);
+    content.appendChild(wrapperContent);
+    section.appendChild(content);
+
+    row.appendChild(section);
+  }
+
+  createCol('autoql-vanilla-notification-setting-display-name', title)
+  createCol('autoql-vanilla-data-alert-list-item-section-frequency', title)
+  createCol('autoql-vanilla-data-alert-list-item-section-state', title)
+  createCol('autoql-vanilla-data-alert-list-item-section-next-check', title)
+  createCol('autoql-vanilla-data-alert-list-item-section-status', status)
+
+  item.appendChild(row);
+
   return item;
 }
