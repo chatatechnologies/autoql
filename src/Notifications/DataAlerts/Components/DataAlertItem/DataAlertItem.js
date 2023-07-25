0271abee25dc30b70bc954e602398f7563139a8d
Oscar Ramirez <oscar@rinro.com.mx>
2023-07-24 20:06:50 -0600

    added DataAlert Cycle Start

--- src/Notifications/DataAlerts/Components/DataAlertItem/DataAlertItem.js
+++ src/Notifications/DataAlerts/Components/DataAlertItem/DataAlertItem.js
@@ -1,5 +1,5 @@
 import './DataAlertItem.scss';
-import { getScheduleFrequencyObject, formatNextScheduleDate } from 'autoql-fe-utils';
+import { getScheduleFrequencyObject, formatNextScheduleDate, resetDateIsFuture, SCHEDULED_TYPE } from 'autoql-fe-utils';
 
 export function DataAlertItem({ dataAlert }) {
   console.log(dataAlert);
@@ -32,14 +32,38 @@ export function DataAlertItem({ dataAlert }) {
     row.appendChild(section);
   }
 
-  const renderDataAlertCycleStart = () => {
-    
+  const hasError = () => {
+    return (
+      dataAlert.status === 'GENERAL_ERROR' ||
+      dataAlert.status === 'EVALUATION_ERROR' ||
+      dataAlert.status === 'DATA_RETURN_ERROR'
+    )
+  }
+
+  const getDataAlertCycleStart = () => {
+    if (hasError()) {
+      return '-'
+    }
+    if (dataAlert.notification_type === SCHEDULED_TYPE) {
+      const nextScheduledDate = formatNextScheduleDate(dataAlert.schedules, true)
+      if (!nextScheduledDate) {
+        return '-'
+      }
+      return `<span>${nextScheduledDate}</span>`
+    }
+
+    if (!dataAlert.reset_date || !resetDateIsFuture(dataAlert)) {
+      const evaluationFrequency = dataAlert.evaluation_frequency ?? DEFAULT_EVALUATION_FREQUENCY
+      return `< ${evaluationFrequency}m`
+    }
+
+    return `<span>${formatResetDate(dataAlert, true)}</span>`
   }
 
   createCol('autoql-vanilla-notification-setting-display-name', title);
   createCol('autoql-vanilla-data-alert-list-item-section-frequency', getScheduleFrequencyObject(dataAlert).displayText);
   createCol('autoql-vanilla-data-alert-list-item-section-state', title);
-  createCol('autoql-vanilla-data-alert-list-item-section-next-check', formatNextScheduleDate(dataAlert.schedules, true));
+  createCol('autoql-vanilla-data-alert-list-item-section-next-check', getDataAlertCycleStart());
   createCol('autoql-vanilla-data-alert-list-item-section-status', status);
 
   item.appendChild(row);
