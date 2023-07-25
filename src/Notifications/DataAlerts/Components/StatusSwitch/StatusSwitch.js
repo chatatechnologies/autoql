9d91baceae72b3aa1e2b846e12b020cbd954758f
Oscar Ramirez <oscar@rinro.com.mx>
2023-07-25 12:15:53 -0600

    Improved swtich styles

--- src/Notifications/DataAlerts/Components/StatusSwitch/StatusSwitch.js
+++ src/Notifications/DataAlerts/Components/StatusSwitch/StatusSwitch.js
@@ -7,16 +7,18 @@ export function StatusSwitch({ status }) {
   const textStatus = document.createElement('span');
   textStatus.classList.add('autql-vanilla-switch-text');
   slider.classList.add('autoql-vanilla-switcher-status');
-  textStatus.textContent = status;
+  textStatus.textContent = status.charAt(0).toUpperCase() + status.slice(1).toLowerCase();
   slider.appendChild(textStatus);
 
   slider.setOnChange(() => {
     if(slider.isChecked()) {
       textStatus.classList.add('left');
       textStatus.classList.remove('right');
+      textStatus.textContent = 'Active';
     } else {
       textStatus.classList.add('right');
       textStatus.classList.remove('left');
+      textStatus.textContent = 'Inactive';
     }
   })
 
