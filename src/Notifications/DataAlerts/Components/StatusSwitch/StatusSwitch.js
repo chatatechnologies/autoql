1fafd746b6cf9293bd1526990b8e518ce887b6de
Oscar Ramirez <oscar@rinro.com.mx>
2023-07-25 11:11:50 -0600

    added switch onChange event

--- src/Notifications/DataAlerts/Components/StatusSwitch/StatusSwitch.js
+++ src/Notifications/DataAlerts/Components/StatusSwitch/StatusSwitch.js
@@ -7,7 +7,26 @@ export function StatusSwitch({ status }) {
   const textStatus = document.createElement('span');
   textStatus.classList.add('autql-vanilla-switch-text');
   slider.classList.add('autoql-vanilla-switcher-status');
-  textStatus.textContent = 'Active';
+  textStatus.textContent = status;
   slider.appendChild(textStatus);
+
+  slider.setOnChange(() => {
+    if(slider.isChecked()) {
+      textStatus.classList.add('left');
+      textStatus.classList.remove('right');
+    } else {
+      textStatus.classList.add('right');
+      textStatus.classList.remove('left');
+    }
+  })
+
+  if(status === 'ACTIVE') {
+    slider.setChecked(true);
+    textStatus.classList.add('left');
+  } else {
+    slider.setChecked(false);
+    textStatus.classList.add('right');
+  }
+
   return slider;
 }
