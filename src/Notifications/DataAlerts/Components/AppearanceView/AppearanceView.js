598724893cc6870a7b310abc6714ed70b49e0ae9
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-05 22:11:22 -0600

    added compose message section

--- src/Notifications/DataAlerts/Components/AppearanceView/AppearanceView.js
+++ src/Notifications/DataAlerts/Components/AppearanceView/AppearanceView.js
@@ -1,26 +1,68 @@
 import './AppearanceView.scss';
+import { NOTEBOOK } from '../../../../Svg';
+import { createIcon } from '../../../../Utils';
 
 export function AppearanceView() {
   const container = document.createElement('div');
   const title = document.createElement('div');
   const wrapper = document.createElement('div');
   const composeMessageSection = document.createElement('div');
+  const formSection = document.createElement('div');
   
-  const titleContainer = document.createElement('div');
-  const titleInputLabel = document.createElement('div');
+  const createInputContainer = ({ inputType, placeholder, icon, label }) => {
+    const container = document.createElement('div');
+    const inputLabel = document.createElement('div');
+    const inputContainer = document.createElement('div');
+    const inputWrapper = document.createElement('div');
+    const input = document.createElement(inputType);
+    inputLabel.textContent = label;
+    inputWrapper.appendChild(input);
+
+    inputContainer.appendChild(inputWrapper);
+    if(icon) {
+      inputWrapper.appendChild(icon);
+      input.classList.add('autoql-vanilla-with-icon');
+    }
+
+    container.appendChild(inputLabel);
+    container.appendChild(inputContainer);
+
+    container.classList.add('autoql-vanilla-input-and-label-container');
+    inputLabel.classList.add('autoql-vanilla-input-label');
+    inputContainer.classList.add('autoql-vanilla-input-container');
+    inputWrapper.classList.add('autoql-vanilla-input-and-icon');
+    input.classList.add('autoql-vanilla-input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', placeholder);
+
+    return container;
+  }
 
   title.textContent = 'Appearance';
-  titleInputLabel.textContent = 'Title';
+
+  const titleSection = createInputContainer({
+    inputType: 'input',
+    placeholder: 'eg. "Budget alert!"',
+    icon: createIcon(NOTEBOOK),
+    label: 'Title'
+  });
+
+  const messageSection = createInputContainer({
+    inputType: 'textarea',
+    placeholder: 'eg. "You have spent 80% of your budget for the month."',
+    label: 'Message (optional)',
+  });
+  messageSection.classList.add('autoql-vanilla-notification-message-input');
 
   title.classList.add('autoql-vanilla-data-alert-setting-section-title');
   wrapper.classList.add('autoql-vanilla-data-alerts-container');
   container.classList.add('autoql-vanilla-data-alert-setting-section');
   composeMessageSection.classList.add('autoql-vanilla-compose-message-section');
-  titleContainer.classList.add('autoql-vanilla-input-and-label-container');
-  titleInputLabel.classList.add('autoql-vanilla-input-label');
+  formSection.classList.add('autoql-vanilla-form-section');
 
-  titleContainer.appendChild(titleInputLabel);
-  composeMessageSection.appendChild(titleContainer);
+  formSection.appendChild(titleSection);
+  formSection.appendChild(messageSection);
+  composeMessageSection.appendChild(formSection);
   wrapper.appendChild(composeMessageSection);
   container.appendChild(title);
   container.appendChild(wrapper);
