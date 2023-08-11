31753fdcd9cdc99a166d7b89289ae45aa9a047c7
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-11 12:04:53 -0600

    added Chip styles

--- src/Notifications/Components/Chip/Chip.js
+++ src/Notifications/Components/Chip/Chip.js
@@ -1,5 +1,7 @@
+import { create } from "d3";
 import { CLOSE_ICON, ICON_LOCK } from "../../../Svg";
 import { createIcon } from "../../../Utils";
+import './Chip.scss'
 
 export function Chip({ data }) {
   const chip = document.createElement('div');
@@ -9,15 +11,19 @@ export function Chip({ data }) {
   const message = document.createElement('strong');
 
   const btnDeleteChip = document.createElement('div');
+  const closeIcon = createIcon(CLOSE_ICON);
+  const lockIcon = createIcon(ICON_LOCK);
 
   chip.classList.add('autoql-vanilla-chip');
   background.classList.add('autoql-vanilla-chip-background');
   content.classList.add('autoql-vanilla-chip-content');
   btnDeleteChip.classList.add('autoql-vanilla-chip-delete-confirm-popover');
+  closeIcon.classList.add('autoql-vanilla-chip-delete-btn');
+  lockIcon.classList.add('autoql-vanilla-lock-icon');
+  
+  btnDeleteChip.appendChild(closeIcon);
 
-  btnDeleteChip.appendChild(createIcon(CLOSE_ICON));
-
-  message.appendChild(createIcon(ICON_LOCK));
+  message.appendChild(lockIcon);
   message.appendChild(document.createTextNode(`${data.show_message}`));
   message.appendChild(document.createTextNode(': '));
   contentWrapper.appendChild(message);
@@ -26,6 +32,6 @@ export function Chip({ data }) {
   content.appendChild(btnDeleteChip);
   chip.appendChild(content);
   chip.appendChild(background);
-  
+
   return chip;
 }
--- src/Notifications/Components/ChipsContainer/ChipsContainer.js
+++ src/Notifications/Components/ChipsContainer/ChipsContainer.js
@@ -1,11 +1,14 @@
 import { Chip } from "../Chip/Chip";
+import './ChipsContainer.scss';
 
 export function ChipsContainer({ filters }) {
-  const container = document.createElement('autoql-vanilla-data-alert-filters-container');
+  const container = document.createElement('div');
 
   filters.map(filter => {
     container.appendChild(new Chip({ data: filter }));
-  })
+  });
+
+  container.classList.add('autoql-vanilla-data-alert-filters-container');
 
   return container;
 }
