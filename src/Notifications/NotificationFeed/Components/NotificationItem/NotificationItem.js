1ab3d6953a7abbfcb3a6e5a993fc36c66f9fbaac
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-15 17:14:48 -0600

    added NotificationItem styles

--- src/Notifications/NotificationFeed/Components/NotificationItem/NotificationItem.js
+++ src/Notifications/NotificationFeed/Components/NotificationItem/NotificationItem.js
@@ -1,11 +1,47 @@
 import { CALENDAR, VERTICAL_DOTS } from "../../../../Svg";
 import { createIcon } from "../../../../Utils";
+import dayjs from '../../../../Utils/dayjsPlugins';
+import './NotificationItem.scss';
 
 export function NotificationItem({ itemData }) {
+  console.log(itemData);
   const item = document.createElement('div');
   item.classList.add('autoql-vanilla-notification-list-item');
 
+  this.getFormattedTimestamp = () => {
+    const timestamp = itemData.created_at;
+
+    const dateDayJS = dayjs.unix(timestamp)
+
+    const time = dateDayJS.format('h:mma')
+    const day = dateDayJS.format('MM-DD-YY')
+
+    const today = dayjs().format('MM-DD-YY')
+    const yesterday = dayjs().subtract(1, 'd').format('MM-DD-YY')
+
+    if (day === today) {
+      return `Today at ${time}`
+    } else if (day === yesterday) {
+      return `Yesterday at ${time}`
+    } else if (dayjs().isSame(dateDayJS, 'year')) {
+      return `${dateDayJS.format('MMMM Do')} at ${time}`
+    }
+    return `${dateDayJS.format('MMMM Do, YYYY')} at ${time}`
+  }
+
+  this.createStrip = () => {
+    const strip = document.createElement('div');
+    strip.classList.add('autoql-vanilla-notification-alert-strip');
+
+    return strip;
+  }
+
   this.createHeader = () => {
+    const {
+      title,
+      message,
+      state
+    } = itemData;
     const header = document.createElement('div');
     const displayNameContainer = document.createElement('div');
     const displayName = document.createElement('div');
@@ -16,7 +52,6 @@ export function NotificationItem({ itemData }) {
     const btnContainer = document.createElement('div');
     const verticalDots = createIcon(VERTICAL_DOTS);
 
-    const strip = document.createElement('div');
 
     header.classList.add('autoql-vanilla-notification-list-item-header');
     displayNameContainer.classList.add('autoql-vanilla-notification-display-name-container');
@@ -24,12 +59,14 @@ export function NotificationItem({ itemData }) {
     description.classList.add('autoql-vanilla-notification-description');
     timestampContainer.classList.add('autoql-vanilla-notification-timestamp-container');
     timestamp.classList.add('autoql-vanilla-notification-timestamp');
-    btnContainer.classList.add('autoql-vanilla-notification-options-btn-container')
+    btnContainer.classList.add('autoql-vanilla-notification-options-btn-container');
     verticalDots.classList.add('autoql-vanilla-notification-options-btn');
-    strip.classList.add('autoql-vanilla-notification-alert-strip')
-    displayName.textContent = 'Test1';
+    
+    displayName.textContent = title;
+    description.textContent = message;
+
     timestamp.appendChild(createIcon(CALENDAR));
-    timestamp.appendChild(document.createTextNode(''));
+    timestamp.appendChild(document.createTextNode(this.getFormattedTimestamp()));
 
     btnContainer.appendChild(verticalDots);
     timestampContainer.appendChild(timestamp);
@@ -40,7 +77,10 @@ export function NotificationItem({ itemData }) {
     header.appendChild(displayNameContainer);
     header.appendChild(btnContainer);
     item.appendChild(header);
-    item.appendChild(strip);
+
+    if(state === 'ACKNOWLEDGED') {
+      item.appendChild(this.createStrip());
+    }
   }
 
   this.createHeader();
