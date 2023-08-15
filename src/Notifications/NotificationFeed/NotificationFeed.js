b024a7b4978951c4f96a041decef8197329306a2
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-15 11:07:29 -0600

    added NotificationFeed base object

---
+++ src/Notifications/NotificationFeed/NotificationFeed.js
@@ -0,0 +1,6 @@
+export function NotificationFeed() {
+  const container = document.createElement('div');
+  container.classList.add('autoql-vanila-notification-list-container');
+
+  return container;
+}
