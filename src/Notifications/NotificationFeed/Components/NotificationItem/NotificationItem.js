79a2efc6d66f72538a7b3fd5f513a93314b4b4cd
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-20 17:19:46 -0600

    Refactor Notification summary

--- src/Notifications/NotificationFeed/Components/NotificationItem/NotificationItem.js
+++ src/Notifications/NotificationFeed/Components/NotificationItem/NotificationItem.js
@@ -1,13 +1,12 @@
 import {
-  constructRTArray,
   dismissNotification,
   fetchNotificationData,
-  DATA_ALERT_OPERATORS,
 } from "autoql-fe-utils";
-import { CALENDAR, CARET_DOWN_ICON, VERTICAL_DOTS, WARNING_TRIANGLE, REFRESH_ICON } from "../../../../Svg";
+import { CALENDAR, CARET_DOWN_ICON, VERTICAL_DOTS, WARNING_TRIANGLE } from "../../../../Svg";
 import { createIcon, getFormattedTimestamp } from "../../../../Utils";
 import './NotificationItem.scss';
 import { NotificationMessageError } from "../NotificationMessageError";
+import { NotificationSummary } from "../NotificationSummary";
 
 const dataAlertErrorName = 'Data Alert Error';
 const DELAY = 0.08;
@@ -38,93 +37,8 @@ export function NotificationItem({ itemData, authentication, index, onClick }) {
     return new NotificationMessageError({ itemData });
   }
 
-  this.getInterpretationChunk = (chunk) => {
-    switch (chunk.c_type) {
-      case 'VALIDATED_VALUE_LABEL': {
-        return ` ${chunk.eng}`
-      }
-      case 'VALUE_LABEL': {
-        return ` ${chunk.eng}`
-      }
-      case 'DELIM': {
-        return `${chunk.eng}`
-      }
-      default: {
-        return ` ${chunk.eng}`
-      }
-    }
-  }
-
-  this.getChunkedInterpretationText = () => {
-    const parsedRT = this.queryResponse?.data?.parsed_interpretation
-    const rtArray = constructRTArray(parsedRT)
-    let rtString = ''
-    rtArray.map((chunk, i) => {
-      rtString = `${rtString}${this.getInterpretationChunk(chunk)}`
-    })
-
-    return rtString.trim()
-  }
-
-  this.hasOperator = () => {
-    return itemData?.expression?.length === 2;
-  }
-
-  this.getTextOperator = (operator) => {
-    if(!operator) return 'New data was detected for the query.';
-    const operatorText = DATA_ALERT_OPERATORS[operator].conditionTextPast;
-
-    return operatorText;
-  } 
-
-  this.createTextOperator = (text) => {
-    const operator = document.createElement('span');
-    const textContent = document.createElement('span');
-
-    textContent.textContent = text;
-    textContent.classList.add('autoql-vanilla-data-alert-condition-statement-operator');
-    operator.appendChild(document.createTextNode(' '));
-    operator.appendChild(textContent);
-    operator.appendChild(document.createTextNode(' '));
-
-    return operator;
-  }
-
-  this.createTerm = (text) => {
-    const term = document.createElement('span');
-    term.classList.add('autoql-vanilla-data-alert-condition-statement-term');
-    term.textContent = `"${text}"`;
-
-    return term;
-  }
-
   this.createSummary = () => {
-    const summaryContainer = document.createElement('div');
-    const summaryLabel = document.createElement('span');
-    
-    summaryLabel.textContent = 'Summary:';
-    summaryContainer.appendChild(summaryLabel);
-    summaryContainer.classList.add('autoql-vanilla-notification-condition-statement');
-    
-    if(this.hasOperator()) {
-      const operator = itemData.expression[0].condition;
-      const termValue = itemData.expression[0].term_value;
-      const secondTermValue = itemData.expression[1].term_value;
-      
-      summaryContainer.appendChild(this.createTerm(termValue));
-      summaryContainer.appendChild(this.createTextOperator(this.getTextOperator(operator)));
-      summaryContainer.appendChild(this.createTerm(secondTermValue));
-      
-    } else {
-      const text = this.getChunkedInterpretationText();
-      const operatorText = this.getTextOperator();
-
-      summaryContainer.appendChild(this.createTextOperator(operatorText));
-      summaryContainer.appendChild(this.createTerm(text));
-    }
-
-
-    return summaryContainer;
+    return new NotificationSummary({ itemData, queryResponse: this.queryResponse });
   }
   
   this.createStrip = () => {
@@ -320,6 +234,5 @@ export function NotificationItem({ itemData, authentication, index, onClick }) {
   item.classList.add('autoql-vanilla-notification-collapsed');
   item.style.animationDelay = DELAY * index + 's';
 
-  
   return item;
 }
