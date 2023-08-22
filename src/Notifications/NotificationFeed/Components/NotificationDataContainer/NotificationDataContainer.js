367fe6ad581fdac84a160b5820fc5049c32b9170
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-21 23:04:01 -0600

    added single value response for NotificationItem

--- src/Notifications/NotificationFeed/Components/NotificationDataContainer/NotificationDataContainer.js
+++ src/Notifications/NotificationFeed/Components/NotificationDataContainer/NotificationDataContainer.js
@@ -9,6 +9,40 @@ export function NotificationDataContainer({ queryResponse }) {
   wrapper.classList.add('autoql-vanilla-notification-chart-container');
   container.classList.add('autoql-vanilla-notification-data-container');
  
+  console.log(queryResponse);
+
+  this.createDataResponse = () => {
+    const responseContainer = document.createElement('div');
+    const singleValue = document.createElement('div');
+    const queryContainer = document.createElement('span');
+    const queryWrapper = document.createElement('strong');
+
+    queryWrapper.textContent = queryResponse?.data?.text;
+    
+    queryContainer.appendChild(queryWrapper)
+    queryContainer.appendChild(document.createTextNode(': '));
+    singleValue.appendChild(queryContainer);
+    singleValue.appendChild(document.createTextNode('test'));
+    responseContainer.appendChild(singleValue);
+
+    singleValue.classList.add('autoql-vanilla-single-value-response');
+    responseContainer.classList.add('autoql-vanilla-single-value-response-flex-container');
+    return responseContainer;
+  }
+
+  this.showResponse = (displayType) => {
+    console.log(displayType);
+    switch(displayType) {
+      case 'data':
+        responseContentContainer.appendChild(this.createDataResponse());
+        break;
+    }
+  }
+
+  const displayType = queryResponse.data.display_type;
+
+  this.showResponse(displayType);
+
   wrapper.appendChild(responseContentContainer);
   container.appendChild(wrapper);
   return container;
