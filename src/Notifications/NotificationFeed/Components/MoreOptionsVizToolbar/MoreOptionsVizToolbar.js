c16be722082cf211cdbf0c3415b03bcb426aa766
Oscar Ramirez <oscar@rinro.com.mx>
2023-08-28 21:08:39 -0600

    added export as png handler

--- src/Notifications/NotificationFeed/Components/MoreOptionsVizToolbar/MoreOptionsVizToolbar.js
+++ src/Notifications/NotificationFeed/Components/MoreOptionsVizToolbar/MoreOptionsVizToolbar.js
@@ -1,4 +1,4 @@
-import { isChartType } from "autoql-fe-utils";
+import { isChartType, svgToPng } from "autoql-fe-utils";
 import { Popup } from "../../../../Popup";
 import { createIcon } from "../../../../Utils";
 import { CLIPBOARD_ICON, EXPORT_PNG_ICON } from "../../../../Svg";
@@ -25,6 +25,25 @@ export function MoreOptionsVizToolbar({ notificationItem }) {
     popup.close();
   }
 
+  this.handleSaveAsPng = async() => {
+    try {
+      popup.close(); 
+      const svg = notificationItem.chartContainer.children[1];
+      if (!svg) {
+          console.warn('Unable to download SVG - no svg was found');
+      }
+
+      const base64Data = await svgToPng(svg, 2);
+      const a = document.createElement('a');
+      a.download = 'Chart.png';
+      a.href = base64Data;
+      a.click();
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  }
+
   menu.classList.add('autoql-vanilla-toolbar-more-options');
   popup.appendChild(menu);
 
@@ -33,6 +52,7 @@ export function MoreOptionsVizToolbar({ notificationItem }) {
     if(isChartType(displayType)) {
       const exportAsPng = this.createOption(EXPORT_PNG_ICON, strings.downloadPNG);
       menu.appendChild(exportAsPng);
+      exportAsPng.onclick = this.handleSaveAsPng;
     } else {
       const copyTableToClipboard = this.createOption(CLIPBOARD_ICON, strings.copyTable);
       menu.appendChild(copyTableToClipboard);
