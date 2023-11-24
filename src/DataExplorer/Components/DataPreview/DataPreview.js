06e840387923a9c6dc37068e65fc7bcd5c12eb9d
Nikki Moore <nmoore@chata.ai>
2023-11-24 10:15:21 -0700

    fix data explorer subject list

--- src/DataExplorer/Components/DataPreview/DataPreview.js
+++ src/DataExplorer/Components/DataPreview/DataPreview.js
@@ -1,46 +1,44 @@
-import { Card } from "../Card";
-import './DataPreview.scss';
+import { Card } from '../Card';
 import { fetchDataPreview } from 'autoql-fe-utils';
-import { DataPreviewTable } from "../DataPreviewTable";
+import { DataPreviewTable } from '../DataPreviewTable';
+
+import './DataPreview.scss';
 
 export function DataPreview({ icon, title, subject, widgetOptions }) {
-  let obj = this;
-  const container = document.createElement('div');
-  const card = new Card({ icon, title, maxHeight: 155 });
-  const {
-    domain,
-    apiKey,
-    token,
-  } = widgetOptions.authentication;
+    console.log({ subjectForDP: subject });
+    let obj = this;
+    const container = document.createElement('div');
+    const card = new Card({ icon, title, maxHeight: 155 });
+    const { domain, apiKey, token } = widgetOptions.authentication;
 
-  container.classList.add('autoql-vanilla-data-explorer-section');
-  container.classList.add('autoql-vanilla-data-preview-section');
+    container.classList.add('autoql-vanilla-data-explorer-section');
+    container.classList.add('autoql-vanilla-data-preview-section');
 
-  container.appendChild(card);
+    container.appendChild(card);
 
-  obj.container = container;
+    obj.container = container;
 
-  obj.getPreview = async() => {
-    card.showLoading();
-    const response = await fetchDataPreview({
-      subject: subject.name,
-      numRows: 5,
-      source: 'data_explorer.data_preview',
-      domain,
-      apiKey,
-      token,
-    });
-    obj.displayResponse(response);
-  }
+    obj.getPreview = async () => {
+        card.showLoading();
+        const response = await fetchDataPreview({
+            subject: subject.context,
+            numRows: 5,
+            source: 'data_explorer.data_preview',
+            domain,
+            apiKey,
+            token,
+        });
+        obj.displayResponse(response);
+    };
 
-  obj.displayResponse = (r) => {
-    card.clearView();
-    const { data } = r.data
-    const table = new DataPreviewTable({ previewResponse: data });
-    card.setContent(table);
-  }
+    obj.displayResponse = (r) => {
+        card.clearView();
+        const { data } = r.data;
+        const table = new DataPreviewTable({ previewResponse: data });
+        card.setContent(table);
+    };
 
-  obj.getPreview();
+    obj.getPreview();
 
-  return obj;
+    return obj;
 }
--- src/DataExplorer/Components/RelatedQueries/RelatedQueries.js
+++ src/DataExplorer/Components/RelatedQueries/RelatedQueries.js
@@ -1,84 +1,88 @@
-import { Card } from "../Card";
-import './RelatedQueries.scss';
-import { fetchDataExplorerSuggestions } from "autoql-fe-utils";
-import { CHATA_BUBBLES_ICON } from "../../../Svg";
-import { createIcon } from "../../../Utils";
+import { Card } from '../Card';
+import { fetchDataExplorerSuggestions } from 'autoql-fe-utils';
+import { CHATA_BUBBLES_ICON } from '../../../Svg';
+import { createIcon } from '../../../Utils';
+
 import './RelatedQueries.scss';
 
-export function RelatedQueries({ 
-  icon, title, widget, subject = {}, plainText, textBarHeight, containerHeight, previewSectionHeight 
+export function RelatedQueries({
+    icon,
+    title,
+    widget,
+    subject = {},
+    plainText,
+    textBarHeight,
+    containerHeight,
+    previewSectionHeight,
 }) {
-  this.currentPage = 1;
-  const container = document.createElement('div');
-  const suggestionList = document.createElement('div');
-  const list = document.createElement('div');
-  const headerHeight = widget.header.clientHeight;
-  const padding = 10;
-  const margin = 40;
-  const titleHeight = 37;
-  const maxHeight = (containerHeight - previewSectionHeight - headerHeight - textBarHeight - titleHeight - margin - padding);
-  let pageNumber = 1;
-  const getRelatedQueries = async({ pageNumber }) => {
-    const relatedQueries = await fetchDataExplorerSuggestions({
-      context: subject.name,
-      text: plainText,
-      skipQueryValidation: true,
-      pageSize: 25,
-      domain,
-      apiKey,
-      token,
-      pageNumber,
-      // TODO: implement safetynet for plain text input
-      // skipQueryValidation: pageNumber > 1,
-      // selectedVL: undefined,
-      // userVLSelection: undefined,
-    });
-    const { items } = relatedQueries.data.data;
+    this.currentPage = 1;
+    const container = document.createElement('div');
+    const suggestionList = document.createElement('div');
+    const list = document.createElement('div');
+    const headerHeight = widget.header.clientHeight;
+    const padding = 10;
+    const margin = 40;
+    const titleHeight = 37;
+    const maxHeight =
+        containerHeight - previewSectionHeight - headerHeight - textBarHeight - titleHeight - margin - padding;
+    let pageNumber = 1;
+    const getRelatedQueries = async ({ pageNumber }) => {
+        const relatedQueries = await fetchDataExplorerSuggestions({
+            context: subject.context,
+            text: plainText,
+            skipQueryValidation: true,
+            pageSize: 25,
+            domain,
+            apiKey,
+            token,
+            pageNumber,
+            // TODO: implement safetynet for plain text input
+            // skipQueryValidation: pageNumber > 1,
+            // selectedVL: undefined,
+            // userVLSelection: undefined,
+        });
+        const { items } = relatedQueries.data.data;
 
-    items.forEach((suggestion) => {
-      const item = document.createElement('div');
-      const icon = createIcon(CHATA_BUBBLES_ICON);
-      const text = document.createElement('div');
-      item.classList.add('autoql-vanilla-query-tip-item');
-      item.classList.add('autoql-vanilla-animated-item');
-      text.classList.add('autoql-vanilla-query-suggestion-text');
+        items.forEach((suggestion) => {
+            const item = document.createElement('div');
+            const icon = createIcon(CHATA_BUBBLES_ICON);
+            const text = document.createElement('div');
+            item.classList.add('autoql-vanilla-query-tip-item');
+            item.classList.add('autoql-vanilla-animated-item');
+            text.classList.add('autoql-vanilla-query-suggestion-text');
 
-      item.onclick = () => {
-        widget.setActiveTab(widget.tabChataUtils);
-        widget.tabsAnimation('flex', 'block');
-        widget.dataExplorer.hide();
-        widget.notificationsAnimation('none');
-        widget.keyboardAnimation(suggestion);
-        widget.options.landingPage = 'data-messenger';
-      }
-      text.appendChild(icon);
-      text.appendChild(document.createTextNode(suggestion));
-      item.appendChild(text);
-      list.appendChild(item);
-    });
-  }
+            item.onclick = () => {
+                widget.setActiveTab(widget.tabChataUtils);
+                widget.tabsAnimation('flex', 'block');
+                widget.dataExplorer.hide();
+                widget.notificationsAnimation('none');
+                widget.keyboardAnimation(suggestion);
+                widget.options.landingPage = 'data-messenger';
+            };
+            text.appendChild(icon);
+            text.appendChild(document.createTextNode(suggestion));
+            item.appendChild(text);
+            list.appendChild(item);
+        });
+    };
 
-  const onScroll = () => {
-    pageNumber++;
-    getRelatedQueries({ pageNumber })
-  }
+    const onScroll = () => {
+        pageNumber++;
+        getRelatedQueries({ pageNumber });
+    };
 
-  const card = new Card({ icon, title, widget, subject, maxHeight, onScroll });
-  const {
-    domain,
-    apiKey,
-    token,
-  } = widget.options.authentication;
-  
-  container.classList.add('autoql-vanilla-data-explorer-section');
-  container.classList.add('autoql-vanilla-query-suggestions-section');
-  list.classList.add('autoql-vanilla-query-suggestion-list');
-  suggestionList.classList.add('autoql-vanilla-data-explorer-query-suggestion-list');
-  suggestionList.appendChild(list);
-  card.setContent(suggestionList);
-  container.appendChild(card);
+    const card = new Card({ icon, title, widget, subject, maxHeight, onScroll });
+    const { domain, apiKey, token } = widget.options.authentication;
 
-  getRelatedQueries(pageNumber);
+    container.classList.add('autoql-vanilla-data-explorer-section');
+    container.classList.add('autoql-vanilla-query-suggestions-section');
+    list.classList.add('autoql-vanilla-query-suggestion-list');
+    suggestionList.classList.add('autoql-vanilla-data-explorer-query-suggestion-list');
+    suggestionList.appendChild(list);
+    card.setContent(suggestionList);
+    container.appendChild(card);
 
-  return container;
+    getRelatedQueries(pageNumber);
+
+    return container;
 }
