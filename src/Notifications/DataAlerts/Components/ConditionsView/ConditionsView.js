32d5f4df43ccef12c2d646435cc3823d8f95f613
Nikki Moore <nmoore@chata.ai>
2024-03-07 14:46:12 -0700

    fix: 400 error on data alert creation

--- src/Notifications/DataAlerts/Components/ConditionsView/ConditionsView.js
+++ src/Notifications/DataAlerts/Components/ConditionsView/ConditionsView.js
@@ -2,160 +2,162 @@ import { ChipsContainer } from '../../../Components/ChipsContainer/ChipsContaine
 import { QueryResultInput } from '../../../Components/QueryResultInput';
 import { Selector } from '../../../Components/Selector/Selector';
 import './ConditionsView.scss';
-import { DATA_ALERT_OPERATORS, NUMBER_TERM_TYPE, QUERY_TERM_TYPE, PERIODIC_TYPE  } from 'autoql-fe-utils';
+import { DATA_ALERT_OPERATORS, NUMBER_TERM_TYPE, QUERY_TERM_TYPE, PERIODIC_TYPE } from 'autoql-fe-utils';
 
 export function ConditionsView({ dataAlert }) {
-  //Trigger alert section
-  const container = document.createElement('div');
-  const title = document.createElement('div');
-  const wrapper = document.createElement('div');
-  const ruleInput = document.createElement('div');
-  const readOnlyContainer = document.createElement('div');
-  const labelContainer = document.createElement('div');
-  const inputLabel = document.createElement('div');
-  const inputContainer = document.createElement('div');
-  const wrapperInputContainer = document.createElement('div');
-  const queryInput = document.createElement('input');
-  
-  this.handleInputType = (option) => {
-    const type = option.value === NUMBER_TERM_TYPE ? 'number' : 'text';
-    termInputValue.setInputType(type);
-  }
+    //Trigger alert section
+    const container = document.createElement('div');
+    const title = document.createElement('div');
+    const wrapper = document.createElement('div');
+    const ruleInput = document.createElement('div');
+    const readOnlyContainer = document.createElement('div');
+    const labelContainer = document.createElement('div');
+    const inputLabel = document.createElement('div');
+    const inputContainer = document.createElement('div');
+    const wrapperInputContainer = document.createElement('div');
+    const queryInput = document.createElement('input');
 
-  this.getOperatorSelectValues = () => {
-    const keys = Object.keys(DATA_ALERT_OPERATORS);
-    return keys.map((key) => {
-      const value = DATA_ALERT_OPERATORS[key];
-      const { displayName, symbol } = value;
-      const displayText = `${displayName} (${symbol})`;
-      return {
-        displayName,
-        displayText,
-        value: key,
-      }
-    })
-  }
-  
-  this.getSecondTermValues = () => {
-    return [
-      {
-        value: NUMBER_TERM_TYPE,
-        displayName: `
+    this.handleInputType = (option) => {
+        const type = option.value === NUMBER_TERM_TYPE ? 'number' : 'text';
+        termInputValue.setInputType(type);
+    };
+
+    this.getOperatorSelectValues = () => {
+        const keys = Object.keys(DATA_ALERT_OPERATORS);
+        return keys.map((key) => {
+            const value = DATA_ALERT_OPERATORS[key];
+            const { displayName, symbol } = value;
+            const displayText = `${displayName} (${symbol})`;
+            return {
+                displayName,
+                displayText,
+                value: key,
+            };
+        });
+    };
+
+    this.getSecondTermValues = () => {
+        return [
+            {
+                value: NUMBER_TERM_TYPE,
+                displayName: `
         <span>
         this <strong>number:</strong>
         </span>
         `,
-      },
-      {
-        value: QUERY_TERM_TYPE,
-        displayName: `
+            },
+            {
+                value: QUERY_TERM_TYPE,
+                displayName: `
         <span>
         the result of this <strong>query:</strong>
         </span>
         `,
-      }
-    ]
-  }
+            },
+        ];
+    };
 
-  const defaultSecondTerm = dataAlert.expression?.[1]?.term_type;
-  const termInputType = defaultSecondTerm === NUMBER_TERM_TYPE ? 'number' : 'text';
-  const inputDefaultValue = dataAlert.expression?.[1]?.term_value;
-  const defaultValueCondition = dataAlert.expression?.[0]?.condition;
-  const termInputValue = new QueryResultInput({ termInputType, inputDefaultValue });
-  const conditionSelect = new Selector({ defaultValue: defaultValueCondition, options: this.getOperatorSelectValues() });
-  const secondTermSelect = new Selector({
-    defaultValue: defaultSecondTerm,
-    options: this.getSecondTermValues(),
-    onChange: this.handleInputType
-  });
+    const defaultSecondTerm = dataAlert.expression?.[1]?.term_type;
+    const termInputType = defaultSecondTerm === NUMBER_TERM_TYPE ? 'number' : 'text';
+    const inputDefaultValue = dataAlert.expression?.[1]?.term_value;
+    const defaultValueCondition = dataAlert.expression?.[0]?.condition;
+    const termInputValue = new QueryResultInput({ termInputType, inputDefaultValue });
+    const conditionSelect = new Selector({
+        defaultValue: defaultValueCondition,
+        options: this.getOperatorSelectValues(),
+    });
+    const secondTermSelect = new Selector({
+        defaultValue: defaultSecondTerm,
+        options: this.getSecondTermValues(),
+        onChange: this.handleInputType,
+    });
 
-  this.createChips = () => {
-    this.chipsContainer = new ChipsContainer({ filters: dataAlert.expression[0].session_filter_locks });
-    if(dataAlert?.expression?.[0]?.session_filter_locks?.length === 0)return;
-    ruleInput.appendChild(this.chipsContainer)
-  }
-  
-  this.createConditionsSection = () => {
-    const conditionWrapper = document.createElement('div');
-    const conditionLabel = document.createElement('div');
-    const ruleContainer = document.createElement('div');
-    const secondInputContainer = document.createElement('div');
-    const ruleSecondInputContainer = document.createElement('div');
-    const secondRuleInput = document.createElement('div');
-    const secondLabelContainer = document.createElement('div');
-    
-    conditionLabel.textContent = 'Meets this condition';
-    
-    conditionLabel.classList.add('autoql-vanilla-input-label');
-    conditionWrapper.classList.add('autoql-vanilla-select-and-label');
-    conditionWrapper.classList.add('autoql-vanilla-rule-condition-select');
-    ruleContainer.classList.add('autoql-vanilla-notification-rule-container');
-    secondInputContainer.classList.add('autoql-vanilla-input-container');
-    secondInputContainer.classList.add('autoql-vanilla-input-large');
-    secondInputContainer.classList.add('autoql-vanilla-input-number');
-    ruleSecondInputContainer.classList.add('autoql-vanilla-rule-second-input-container');
-    secondRuleInput.classList.add('autoql-vanilla-rule-input');
-    secondLabelContainer.classList.add('autoql-vanilla-input-and-label-container');
-    
-    conditionWrapper.appendChild(conditionLabel);
-    conditionWrapper.appendChild(conditionSelect);
-    secondInputContainer.appendChild(secondTermSelect);
-    secondInputContainer.appendChild(termInputValue);
-    secondLabelContainer.appendChild(secondInputContainer);
-    secondRuleInput.appendChild(secondLabelContainer);
-    ruleSecondInputContainer.appendChild(secondRuleInput);
-    ruleContainer.appendChild(conditionWrapper);
-    ruleContainer.appendChild(ruleSecondInputContainer);
-    wrapper.appendChild(ruleContainer);
-  }
+    this.createChips = () => {
+        this.chipsContainer = new ChipsContainer({ filters: dataAlert.expression[0].session_filter_locks });
+        if (dataAlert?.expression?.[0]?.session_filter_locks?.length === 0) return;
+        ruleInput.appendChild(this.chipsContainer);
+    };
 
-  title.textContent = 'Conditions';
-  inputLabel.textContent = 'Trigger Alert when this query';
-  queryInput.setAttribute('type', 'text');
-  queryInput.setAttribute('value', dataAlert.data_return_query);
-  queryInput.setAttribute('disabled', 'true');
-  queryInput.setAttribute('readonly', 'true');
+    this.createConditionsSection = () => {
+        const conditionWrapper = document.createElement('div');
+        const conditionLabel = document.createElement('div');
+        const ruleContainer = document.createElement('div');
+        const secondInputContainer = document.createElement('div');
+        const ruleSecondInputContainer = document.createElement('div');
+        const secondRuleInput = document.createElement('div');
+        const secondLabelContainer = document.createElement('div');
 
-  wrapper.classList.add('autoql-vanilla-data-alerts-container');
-  container.classList.add('autoql-vanilla-data-alert-setting-section');
-  title.classList.add('autoql-vanilla-data-alert-setting-section-title');
-  ruleInput.classList.add('autoql-vanilla-rule-input');
-  readOnlyContainer.classList.add('autoql-vanilla-data-alert-rule-query-readonly-container');
-  labelContainer.classList.add('autoql-vanilla-input-and-label-container');
-  inputLabel.classList.add('autoql-vanilla-input-label');
-  inputContainer.classList.add('autoql-vanilla-input-container');
-  inputContainer.classList.add('autoql-vanilla-input-large');
-  wrapperInputContainer.classList.add('autoql-vanilla-input-and-icon');
-  queryInput.classList.add('autoql-vanilla-input');
-  
-  wrapperInputContainer.appendChild(queryInput);
-  inputContainer.appendChild(wrapperInputContainer);
-  labelContainer.appendChild(inputLabel);
-  labelContainer.appendChild(inputContainer);
-  readOnlyContainer.appendChild(labelContainer);
-  ruleInput.appendChild(readOnlyContainer);
-  wrapper.appendChild(ruleInput);
-  container.appendChild(title);
-  container.appendChild(wrapper);
+        conditionLabel.textContent = 'Meets this condition';
 
-  container.getValues = () => {
-    const { expression } = dataAlert;
-    if(dataAlert.notification_type !== PERIODIC_TYPE) {
-      expression[0].condition = conditionSelect.value;
-      expression[1].term_type = secondTermSelect.value;
-      expression[1].term_value = termInputValue.getValue();
+        conditionLabel.classList.add('autoql-vanilla-input-label');
+        conditionWrapper.classList.add('autoql-vanilla-select-and-label');
+        conditionWrapper.classList.add('autoql-vanilla-rule-condition-select');
+        ruleContainer.classList.add('autoql-vanilla-notification-rule-container');
+        secondInputContainer.classList.add('autoql-vanilla-input-container');
+        secondInputContainer.classList.add('autoql-vanilla-input-large');
+        secondInputContainer.classList.add('autoql-vanilla-input-number');
+        ruleSecondInputContainer.classList.add('autoql-vanilla-rule-second-input-container');
+        secondRuleInput.classList.add('autoql-vanilla-rule-input');
+        secondLabelContainer.classList.add('autoql-vanilla-input-and-label-container');
+
+        conditionWrapper.appendChild(conditionLabel);
+        conditionWrapper.appendChild(conditionSelect);
+        secondInputContainer.appendChild(secondTermSelect);
+        secondInputContainer.appendChild(termInputValue);
+        secondLabelContainer.appendChild(secondInputContainer);
+        secondRuleInput.appendChild(secondLabelContainer);
+        ruleSecondInputContainer.appendChild(secondRuleInput);
+        ruleContainer.appendChild(conditionWrapper);
+        ruleContainer.appendChild(ruleSecondInputContainer);
+        wrapper.appendChild(ruleContainer);
+    };
+
+    title.textContent = 'Conditions';
+    inputLabel.textContent = 'Trigger Alert when this query';
+    queryInput.setAttribute('type', 'text');
+    queryInput.setAttribute('disabled', 'true');
+    queryInput.setAttribute('readonly', 'true');
+
+    wrapper.classList.add('autoql-vanilla-data-alerts-container');
+    container.classList.add('autoql-vanilla-data-alert-setting-section');
+    title.classList.add('autoql-vanilla-data-alert-setting-section-title');
+    ruleInput.classList.add('autoql-vanilla-rule-input');
+    readOnlyContainer.classList.add('autoql-vanilla-data-alert-rule-query-readonly-container');
+    labelContainer.classList.add('autoql-vanilla-input-and-label-container');
+    inputLabel.classList.add('autoql-vanilla-input-label');
+    inputContainer.classList.add('autoql-vanilla-input-container');
+    inputContainer.classList.add('autoql-vanilla-input-large');
+    wrapperInputContainer.classList.add('autoql-vanilla-input-and-icon');
+    queryInput.classList.add('autoql-vanilla-input');
+
+    wrapperInputContainer.appendChild(queryInput);
+    inputContainer.appendChild(wrapperInputContainer);
+    labelContainer.appendChild(inputLabel);
+    labelContainer.appendChild(inputContainer);
+    readOnlyContainer.appendChild(labelContainer);
+    ruleInput.appendChild(readOnlyContainer);
+    wrapper.appendChild(ruleInput);
+    container.appendChild(title);
+    container.appendChild(wrapper);
+
+    container.getValues = () => {
+        const { expression } = dataAlert;
+        if (dataAlert.notification_type !== PERIODIC_TYPE) {
+            expression[0].condition = conditionSelect.value;
+            expression[1].term_type = secondTermSelect.value;
+            expression[1].term_value = termInputValue.getValue();
+        }
+        return { expression, session_filter_locks: this.chipsContainer.getValues() };
+    };
+
+    container.isValid = () => {
+        return termInputValue.getValue() !== '';
+    };
+
+    if (dataAlert.notification_type !== PERIODIC_TYPE) {
+        this.createConditionsSection();
     }
-    return { expression, session_filter_locks: this.chipsContainer.getValues() }
-  }
+    this.createChips();
 
-  container.isValid = () => {
-    return termInputValue.getValue() !== '';
-  }
-
-  if(dataAlert.notification_type !== PERIODIC_TYPE ) {
-    this.createConditionsSection();
-  }  
-  this.createChips();
-
-  return container;
+    return container;
 }
--- src/Notifications/DataAlerts/Components/DataAlertEditModal/DataAlertEditModal.js
+++ src/Notifications/DataAlerts/Components/DataAlertEditModal/DataAlertEditModal.js
@@ -1,143 +1,142 @@
-import { ChataConfirmDialog } from "../../../Components/ChataConfirmDialog/ChataConfirmDialog";
-import { Modal } from "../../../../Modal";
+import { ChataConfirmDialog } from '../../../Components/ChataConfirmDialog/ChataConfirmDialog';
+import { Modal } from '../../../../Modal';
 import './DataAlertEditModal.scss';
-import { ConditionsView } from "../ConditionsView/ConditionsView";
-import { TimingView } from "../TimingView";
-import { AppearanceView } from "../AppearanceView";
-import { updateDataAlert } from "autoql-fe-utils";
-import { AntdMessage } from "../../../../Antd";
+import { ConditionsView } from '../ConditionsView/ConditionsView';
+import { TimingView } from '../TimingView';
+import { AppearanceView } from '../AppearanceView';
+import { updateDataAlert } from 'autoql-fe-utils';
+import { AntdMessage } from '../../../../Antd';
 
 export function DataAlertEditModal({ dataAlertItem, dataAlert, authentication }) {
-  const btnDelete = document.createElement('button');
-  const btnCancel = document.createElement('button');
-  const btnSave = document.createElement('button');
-  const modalFooter = document.createElement('div');
-  const buttonContainerRight = document.createElement('div');
-  const buttonContainerLeft = document.createElement('div');
-  const spinner = document.createElement('div');
-  const container = document.createElement('div');
-  const btnSaveString = document.createElement('span');
-  
-  const conditionsView = new ConditionsView({ dataAlert });
-  const timingView = new TimingView({ dataAlert });
-  const appearanceView = new AppearanceView({ dataAlert });
+    const btnDelete = document.createElement('button');
+    const btnCancel = document.createElement('button');
+    const btnSave = document.createElement('button');
+    const modalFooter = document.createElement('div');
+    const buttonContainerRight = document.createElement('div');
+    const buttonContainerLeft = document.createElement('div');
+    const spinner = document.createElement('div');
+    const container = document.createElement('div');
+    const btnSaveString = document.createElement('span');
 
-  container.classList.add('autoql-vanilla-data-alert-settings-modal-content');
-  spinner.classList.add('autoql-vanilla-spinner-loader');
-  spinner.classList.add('hidden');
+    const conditionsView = new ConditionsView({ dataAlert });
+    const timingView = new TimingView({ dataAlert });
+    const appearanceView = new AppearanceView({ dataAlert });
 
-  btnCancel.textContent = 'Cancel';
-  btnCancel.classList.add('autoql-vanilla-chata-btn');
-  btnCancel.classList.add('autoql-vanilla-large');
-  btnCancel.classList.add('autoql-vanilla-default');
-  btnCancel.classList.add('autoql-vanilla-btn-no-border');
+    container.classList.add('autoql-vanilla-data-alert-settings-modal-content');
+    spinner.classList.add('autoql-vanilla-spinner-loader');
+    spinner.classList.add('hidden');
 
-  btnDelete.textContent = 'Delete Data Alert';
-  btnDelete.classList.add('autoql-vanilla-chata-btn');
-  btnDelete.classList.add('autoql-vanilla-large');
-  btnDelete.classList.add('autoql-vanilla-danger');
+    btnCancel.textContent = 'Cancel';
+    btnCancel.classList.add('autoql-vanilla-chata-btn');
+    btnCancel.classList.add('autoql-vanilla-large');
+    btnCancel.classList.add('autoql-vanilla-default');
+    btnCancel.classList.add('autoql-vanilla-btn-no-border');
 
-  btnSave.appendChild(spinner);
-  btnSave.appendChild(btnSaveString);
-  btnSaveString.textContent = 'Save Changes';
-  btnSaveString.classList.add('autoql-vanilla-btn-text-wrapper');
-  btnSave.classList.add('autoql-vanilla-chata-btn');
-  btnSave.classList.add('autoql-vanilla-large');
-  btnSave.classList.add('autoql-vanilla-primary');
+    btnDelete.textContent = 'Delete Data Alert';
+    btnDelete.classList.add('autoql-vanilla-chata-btn');
+    btnDelete.classList.add('autoql-vanilla-large');
+    btnDelete.classList.add('autoql-vanilla-danger');
 
-  modalFooter.classList.add('autoql-vanilla-modal-footer');
+    btnSave.appendChild(spinner);
+    btnSave.appendChild(btnSaveString);
+    btnSaveString.textContent = 'Save Changes';
+    btnSaveString.classList.add('autoql-vanilla-btn-text-wrapper');
+    btnSave.classList.add('autoql-vanilla-chata-btn');
+    btnSave.classList.add('autoql-vanilla-large');
+    btnSave.classList.add('autoql-vanilla-primary');
 
-  buttonContainerLeft.appendChild(btnDelete);
-  buttonContainerRight.appendChild(btnCancel);
-  buttonContainerRight.appendChild(btnSave);
-  
-  modalFooter.appendChild(buttonContainerLeft);
-  modalFooter.appendChild(buttonContainerRight);
+    modalFooter.classList.add('autoql-vanilla-modal-footer');
 
-  const confirmDialogProps = {
-    title: 'Are you sure you want to leave this page?',
-    message: 'All unsaved changes will be lost.',
-    onDiscard: () => {
-      modal.closeAnimation();
-      setTimeout(() => {
-          modal.hideContainer();
-      }, 250);
-    }
-  }
+    buttonContainerLeft.appendChild(btnDelete);
+    buttonContainerRight.appendChild(btnCancel);
+    buttonContainerRight.appendChild(btnSave);
 
-  const onDiscard = () => {
-    new ChataConfirmDialog({ ...confirmDialogProps });
-  }
+    modalFooter.appendChild(buttonContainerLeft);
+    modalFooter.appendChild(buttonContainerRight);
 
-  var modal = new Modal(
-    {
-        withFooter: true,
-        destroyOnClose: true,
-    },
-    () => {},
-    onDiscard
-  );
-  
-  const deleteDataAlertItemHandler = () => {
-    ChataConfirmDialog({
-      title: 'Are you sure you want to delete this Data Alert?',
-      message: 'You will no longer be notified about these changes in your data.',
-      cancelString: 'Go Back',
-      discardString: 'Delete',
-      onDiscard: async () => {
-        const response = await deleteDataAlert(id, authentication);
-        if(response.status === 200) {
-          new AntdMessage('Data Alert was successfully deleted.', 3000);
-        } else {
-          new AntdMessage('Error', 3000);
-        }
+    const confirmDialogProps = {
+        title: 'Are you sure you want to leave this page?',
+        message: 'All unsaved changes will be lost.',
+        onDiscard: () => {
+            modal.closeAnimation();
+            setTimeout(() => {
+                modal.hideContainer();
+            }, 250);
+        },
+    };
+
+    const onDiscard = () => {
+        new ChataConfirmDialog({ ...confirmDialogProps });
+    };
+
+    var modal = new Modal(
+        {
+            withFooter: true,
+            destroyOnClose: true,
+        },
+        () => {},
+        onDiscard,
+    );
+
+    const deleteDataAlertItemHandler = () => {
+        ChataConfirmDialog({
+            title: 'Are you sure you want to delete this Data Alert?',
+            message: 'You will no longer be notified about these changes in your data.',
+            cancelString: 'Go Back',
+            discardString: 'Delete',
+            onDiscard: async () => {
+                const response = await deleteDataAlert(id, authentication);
+                if (response.status === 200) {
+                    new AntdMessage('Data Alert was successfully deleted.', 3000);
+                } else {
+                    new AntdMessage('Error', 3000);
+                }
+                modal.close();
+            },
+        });
+    };
+
+    btnCancel.onclick = onDiscard;
+    btnDelete.onclick = deleteDataAlertItemHandler;
+
+    btnSave.onclick = async () => {
+        spinner.classList.remove('hidden');
+        btnSave.setAttribute('disabled', 'true');
+        const newValues = {
+            id: dataAlert.id,
+            ...conditionsView.getValues(),
+            ...appearanceView.getValues(),
+            ...timingView.getValues(),
+        };
+        const response = await updateDataAlert({ dataAlert: newValues, ...authentication });
         modal.close();
-      }
-    })
-  }
+        if (response.status === 200) {
+            new AntdMessage('Data Alert updated!', 2500);
+            if (!dataAlertItem) {
+                dataAlertItem.setDataAlert(response.data.data);
+            }
+        } else {
+            new AntdMessage('Error', 2500);
+        }
+    };
 
-  btnCancel.onclick = onDiscard;
-  btnDelete.onclick = deleteDataAlertItemHandler;
+    container.addEventListener('keyup', () => {
+        if (!conditionsView.isValid() || !appearanceView.isValid()) {
+            btnSave.disabled = true;
+            btnSave.classList.add('autoql-vanilla-disabled');
+        } else {
+            btnSave.classList.remove('autoql-vanilla-disabled');
+            btnSave.disabled = false;
+        }
+    });
 
-  btnSave.onclick = async () => {
-    spinner.classList.remove('hidden');
-    btnSave.setAttribute('disabled', 'true');
-    const newValues = {
-      data_return_query: dataAlert.data_return_query,
-      id: dataAlert.id,
-      ...conditionsView.getValues(),
-      ...appearanceView.getValues(),
-      ...timingView.getValues(),
-    }
-    const response = await updateDataAlert({ dataAlert: newValues, ...authentication });
-    modal.close();
-    if(response.status === 200) {
-      new AntdMessage('Data Alert updated!', 2500);
-      if(!dataAlertItem) {
-        dataAlertItem.setDataAlert(response.data.data);
-      }
-    }else {
-      new AntdMessage('Error', 2500);
-    }
-  }
+    container.appendChild(conditionsView);
+    container.appendChild(timingView);
+    container.appendChild(appearanceView);
+    modal.addView(container);
+    modal.chataModal.classList.add('autoql-vanilla-modal-full-size');
+    modal.setTitle('Edit Data Alert Settings');
+    modal.addFooterElement(modalFooter);
 
-  container.addEventListener('keyup', () => {
-    if(!conditionsView.isValid() || !appearanceView.isValid()) {
-      btnSave.disabled = true;
-      btnSave.classList.add('autoql-vanilla-disabled');
-    } else {
-      btnSave.classList.remove('autoql-vanilla-disabled');
-      btnSave.disabled = false;
-    }
-  })
-
-  container.appendChild(conditionsView);
-  container.appendChild(timingView);
-  container.appendChild(appearanceView);
-  modal.addView(container);
-  modal.chataModal.classList.add('autoql-vanilla-modal-full-size');
-  modal.setTitle('Edit Data Alert Settings');
-  modal.addFooterElement(modalFooter);
-
-  return modal;
+    return modal;
 }
